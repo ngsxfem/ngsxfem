cd3d42ca74e1f5aa0326ab96b19d4c1a8baa633a
schruste@pummelzacken <christoph.lehrenfeld@rwth-aachen.de>
2014-03-13 01:05:58 +0100

    * advanced conditioning testing script

--- xfem/pde_demos/conditioning-test/clean.bash
+++ xfem/pde_demos/conditioning-test/clean.bash
@@ -2,3 +2,6 @@ rm *.out *.ini -f
 rm out* -f 
 rm *.prof -f
 rm *.pde -f
+rm *.pdf -f
+rm *.temp -f
+rm *.temp2 -f
--- xfem/pde_demos/conditioning-test/cutscript.bash
+++ xfem/pde_demos/conditioning-test/cutscript.bash
@@ -9,22 +9,56 @@ echo "# ---"  > out.stdmesh.its.BJ
 echo "# ---"  > out.stdmesh.its.BDDC
 echo "# ---"  > out.stdmesh.its.BBDDC
 echo "# ---"  > out.stdmesh.err
-LOW=3000
-UP=3030
-# LOW=1
-# UP=4
-EXP=$LOW
+
 LAM=2
-while [  $EXP -lt $UP ]; do
-#while [  $LAM -lt $UP ]; do
-    echo "$LOW / $EXP / $UP"
+ANEG=2
+
+#***A***
+# POS=370500
+#***B***
+POS=470500
+#***C***
+# POS=370500
+
+#***A***
+# LOW=3705
+# UP=371000
+# POS=$LOW
+#***B***
+LOW=1
+UP=1700000
+LAM=$LOW
+#***C***
+# LOW=1
+# UP=2000000
+# ANEG=$LOW
+
+#***A***
+# sed "s/calcaxis/0.37+(\$0)*0.00001/g" stdmesh.gpl.template > stdmesh.gpl.temp
+# sed "s/hiddenlogscalex/\#/g" stdmesh.gpl.temp > stdmesh.gpl
+#***B***
+sed "s/calcaxis/2**(\$0)/g" stdmesh.gpl.template > stdmesh.gpl.temp
+sed "s/hiddenlogscalex//g" stdmesh.gpl.temp > stdmesh.gpl
+#***C***
+# sed "s/calcaxis/2**(\$0-10)/g" stdmesh.gpl.template > stdmesh.gpl.temp
+# sed "s/hiddenlogscalex//g" stdmesh.gpl.temp > stdmesh.gpl
+
+
+#***A***
+#while [  $POS -lt $UP ]; do
+#***B***
+while [  $LAM -lt $UP ]; do
+#***C***
+#while [  $ANEG -lt $UP ]; do
+    echo "POS = $POS"
     echo "LAM = $LAM"
-    sed "s/var/$EXP/g" stdmesh_test.pde.template > stdmesh_test.pde.temp
-    sed "s/setlam/$LAM/g" stdmesh_test.pde.temp > stdmesh_test.pde
+    echo "ANEG = $ANEG"
+    sed "s/var/$POS/g" stdmesh_test.pde.template > stdmesh_test.pde.temp
+    sed "s/setlam/$LAM/g" stdmesh_test.pde.temp > stdmesh_test.pde.temp2
+    sed "s/ANEG/$ANEG/g" stdmesh_test.pde.temp2 > stdmesh_test.pde
     ngs stdmesh_test.pde > out.stdmesh
-    # sed "s/var/5000/g" stdmesh_test.pde.template > stdmesh_test.pde
     # # echo "numproc quit npquit" >> stdmesh_test.pde
-    # netgen -pdefile=stdmesh_test.pde -solve=$EXP | tee out.stdmesh
+    # netgen -pdefile=stdmesh_test.pde -solve=$POS | tee out.stdmesh
     cat out.stdmesh | grep "condition number:" | tail -n 2 | head -n 1 >> out.stdmesh.cond.A
     cat out.stdmesh | grep " Condition" | tail -n 4 | head -n 1 >> out.stdmesh.cond.BDDCinvA
     cat out.stdmesh | grep " Condition" | tail -n 4 |head -n 2 | tail -n 1 >> out.stdmesh.cond.BBDDCinvA
@@ -34,6 +68,7 @@ while [  $EXP -lt $UP ]; do
     cat out.stdmesh | grep "Iterations:" | tail -n 4 |head -n 2 | tail -n 1 >> out.stdmesh.its.BBDDC
     cat out.stdmesh | grep "Iterations:" | tail -n 4 |tail -n 2 | head -n 1 >> out.stdmesh.its.BJ
     cat out.stdmesh | grep "Iterations:" | tail -n 4 |tail -n 1  >> out.stdmesh.its.J
+    cat out.stdmesh | grep "l2_n" -A 2 | tail -n 1 >> out.stdmesh.err
     cat out.stdmesh | grep "condition number:" | tail -n 2 |head -n 1
     cat out.stdmesh | grep " Condition" | tail -n 4 |head -n 1
     cat out.stdmesh | grep " Condition" | tail -n 4 |head -n 2 | tail -n 1
@@ -43,10 +78,15 @@ while [  $EXP -lt $UP ]; do
     cat out.stdmesh | grep "Iterations:" | tail -n 4 |head -n 2 | tail -n 1
     cat out.stdmesh | grep "Iterations:" | tail -n 4 |tail -n 2 | head -n 1
     cat out.stdmesh | grep "Iterations:" | tail -n 4 |tail -n 1 
+    cat out.stdmesh | grep "l2_n" -A 2 | tail -n 1
 #    cat out.stdmesh | grep "l2_n" -A 1 | tail -n 1 >> out.stdmesh.err
 #    cat out.stdmesh | grep "l2_n" -A 1 | tail -n 1
-let EXP=EXP+1
-#let LAM=LAM*2
+#***A***
+#let POS=POS+10
+#***B***
+let LAM=LAM*2
+#***A***
+#let ANEG=ANEG*2
 done
 
 gnuplot stdmesh.gpl
