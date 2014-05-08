b63c752e3bc71ea959369e5c30852c28cdea71ad
schruste@pummelzacken <christoph.lehrenfeld@rwth-aachen.de>
2014-05-08 14:54:42 +0200

    * conditioning test runs for new geometry...

--- xfem/pde_demos/conditioning-test/cutscript.bash
+++ xfem/pde_demos/conditioning-test/cutscript.bash
@@ -10,55 +10,58 @@ echo "# ---"  > out.stdmesh.its.BDDC
 echo "# ---"  > out.stdmesh.its.BBDDC
 echo "# ---"  > out.stdmesh.err
 
+#PDE_TEMPLATE="stdmesh_test.pde.template"
+PDE_TEMPLATE="roundedsquare.pde.template"
+
 LAM=2
-ANEG=2048
-STEPPOS=25
+ANEG=1024
+STEPPOS=2
 #***A***
-# POS=370500
+POS=0
 #***B***
-POS=470500
+# POS=470500
 #***C***
 # POS=370500
 
 #***A***
-# LOW=563500
-# UP=566001
-# POS=$LOW
+LOW=0
+UP=8000
+POS=$LOW
 #***B***
-LOW=1
-UP=6500000
-LAM=$LOW
+# LOW=1
+# UP=6500000
+# LAM=$LOW
 #***C***
 # LOW=1
 # UP=2000000
 # ANEG=$LOW
 
 #***A***
-# sed "s/calcaxis/($LOW\/1e6)+(\$0)*($STEPPOS\/1e6)/g" stdmesh.gpl.template > stdmesh.gpl.temp
-# sed "s/hiddenlogscalex/\#/g" stdmesh.gpl.temp > stdmesh.gpl
+sed "s/calcaxis/($LOW\/1e6)+(\$0)*($STEPPOS\/1e6)/g" stdmesh.gpl.template > stdmesh.gpl.temp
+sed "s/hiddenlogscalex/\#/g" stdmesh.gpl.temp > stdmesh.gpl
 #***B***
-sed "s/calcaxis/2**(\$0)/g" stdmesh.gpl.template > stdmesh.gpl.temp
-sed "s/hiddenlogscalex//g" stdmesh.gpl.temp > stdmesh.gpl
+# sed "s/calcaxis/2**(\$0)/g" stdmesh.gpl.template > stdmesh.gpl.temp
+# sed "s/hiddenlogscalex//g" stdmesh.gpl.temp > stdmesh.gpl
 #***C***
 # sed "s/calcaxis/2**(\$0-10)/g" stdmesh.gpl.template > stdmesh.gpl.temp
 # sed "s/hiddenlogscalex//g" stdmesh.gpl.temp > stdmesh.gpl
 
 
 #***A***
-# while [  $POS -lt $UP ]; do
+while [  $POS -lt $UP ]; do
 #***B***
-while [  $LAM -lt $UP ]; do
+# while [  $LAM -lt $UP ]; do
 #***C***
 # while [  $ANEG -lt $UP ]; do
     echo "POS = $POS"
     echo "LAM = $LAM"
     echo "ANEG = $ANEG / 1024"
-    sed "s/var/$POS/g" stdmesh_test.pde.template > stdmesh_test.pde.temp
-    sed "s/setlam/$LAM/g" stdmesh_test.pde.temp > stdmesh_test.pde.temp2
-    sed "s/ANEG/$ANEG/g" stdmesh_test.pde.temp2 > stdmesh_test.pde
-    ngs stdmesh_test.pde > out.stdmesh
-    # # echo "numproc quit npquit" >> stdmesh_test.pde
-    # netgen -pdefile=stdmesh_test.pde -solve=$POS | tee out.stdmesh
+    sed "s/var/$POS/g" $PDE_TEMPLATE > temporary.pde.temp
+    sed "s/setlam/$LAM/g" temporary.pde.temp > temporary.pde.temp2
+    sed "s/ANEG/$ANEG/g" temporary.pde.temp2 > temporary.pde
+    ngs temporary.pde > out.stdmesh
+    # # echo "numproc quit npquit" >> temporary.pde
+    # netgen -pdefile=temporary.pde -solve=$POS | tee out.stdmesh
     cat out.stdmesh | grep "condition number:" | tail -n 2 | head -n 1 >> out.stdmesh.cond.A
     cat out.stdmesh | grep " Condition" | tail -n 4 | head -n 1 >> out.stdmesh.cond.BDDCinvA
     cat out.stdmesh | grep " Condition" | tail -n 4 |head -n 2 | tail -n 1 >> out.stdmesh.cond.BBDDCinvA
@@ -82,9 +85,9 @@ while [  $LAM -lt $UP ]; do
 #    cat out.stdmesh | grep "l2_n" -A 1 | tail -n 1 >> out.stdmesh.err
 #    cat out.stdmesh | grep "l2_n" -A 1 | tail -n 1
 #***A***
-# let POS=POS+STEPPOS
+    let POS=POS+STEPPOS
 #***B***
-let LAM=LAM*2
+# let LAM=LAM*2
 #***C***
 # let ANEG=ANEG*2
 done
