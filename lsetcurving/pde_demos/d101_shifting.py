22464561ee6e35d9c4a44fd044bb4f037ef46dd2
Christoph Lehrenfeld <christoph.lehrenfeld@rwth-aachen.de>
2015-12-29 16:48:53 +0100

    shifting stuff...

--- lsetcurving/pde_demos/d101_shifting.py
+++ lsetcurving/pde_demos/d101_shifting.py
@@ -17,39 +17,46 @@ mesh = Mesh(geom.GenerateMesh (mp=mp))
 
 # lset_stats = StatisticContainer()
 # deform_stats = StatisticContainer()
-order = 2
+order = 4
 R = 2.0/3.0
-nlvl = 7
-nsamps = 41
+nlvl = 8
+nsamps = 801
 error_tab = [[None for i in range(0,nlvl)] for j in range(0,nsamps)]
+error_tab2 = [[None for i in range(0,nsamps)] for j in range(0,nlvl)]
+x_tab = [None for j in range(0,nsamps)]
 
-print("error_tab = \n", error_tab)
+#print("error_tab = \n", error_tab)
+f = open('out', 'w')
 
 for samp in range(0,nsamps):
+
+  f.write ("samp = " + str(samp) + " / " + str(nsamps) + "\n")
+  f.flush()
+  x0 = samp/(nsamps-1)*0.1-0.05;
+  print ("x0 = ", x0)
+  x_tab[samp]=x0
+  # x0=0
+  y0=0
+  levelsetstring = "sqrt((x-("+str(x0)+"))*(x-("+str(x0)+"))+(y-("+str(y0)+"))*(y-("+str(y0)+")))-(0.5+0.1*sin(8*atan2(x-("+str(x0)+"),y-("+str(y0)+"))))"
+  # print(levelsetstring)
+  levelset = VariableCF(levelsetstring)
+
+
   geom = SplineGeometry("d01_testgeom.in2d")
   mp = MeshingParameters (maxh=0.1)
   mesh = Mesh(geom.GenerateMesh (mp=mp))
-  lsetmeshadap = LevelSetMeshAdaptation(mesh, order=order, threshold=0.1)
+  lsetmeshadap = LevelSetMeshAdaptation(mesh, order=order, threshold=0.2)
   for lvl in range(0,nlvl):
-    x0 = samp/(nsamps-1)*0.1-0.05;
-    print ("x0 = ", x0)
-    # x0=0
-    y0=0
-    levelsetstring = "sqrt((x-("+str(x0)+"))*(x-("+str(x0)+"))+(y-("+str(y0)+"))*(y-("+str(y0)+")))-(0.5+0.1*sin(8*atan2(x-("+str(x0)+"),y-("+str(y0)+"))))"
-    print(levelsetstring)
-    levelset = VariableCF(levelsetstring)
-    
-    
-    
     deformation = lsetmeshadap.CalcDeformation(levelset)
     maxdist = lsetmeshadap.CalcMaxDistance(levelset);
     
     # Draw(deformation)
     # Draw(lsetmeshadap.lset_p1)
-    
+   
     print ("maxdist:",maxdist)
   
     error_tab[samp][lvl] = maxdist
+    error_tab2[lvl][samp] = maxdist
     lsetmeshadap.MarkForRefinement(levelset,1e-99,absolute=False)
     mesh.Refine()
 
@@ -63,5 +70,13 @@ plt.legend()
 plt.ion()
 plt.show()
 
+print("error_tab = \n", error_tab)
+print("error_tab2 = \n", error_tab2)
+print("x_tab = \n", x_tab)
+f.write("\nerror_tab = \n"+ str(error_tab))
+f.write("\nerror_tab2 = \n"+ str(error_tab2))
+f.write("\nx_tab = \n"+ str(x_tab))
+
+f.close()
 
 input("finished")  
