4046f148b7d790fcebbc923d90d5695e30f8d2db
Christoph Lehrenfeld <christoph.lehrenfeld@rwth-aachen.de>
2016-02-27 11:29:48 +0100

    tracefem example: more content

--- tracefem/pde_demos/simple.py
+++ tracefem/pde_demos/simple.py
@@ -8,20 +8,24 @@ from ngsolve import *
 import libngsxfem_py.xfem as xfem                                 
 
 # geometry stuff
-# unit square [-1,1]^2 (boundary index 1 everywhere)
-# from netgen.geom2d import SplineGeometry
-# square = SplineGeometry()
-# square.AddRectangle([-1,-1],[1,1],bc=1)
+# unit square [-2,2]^2 (boundary index 1 everywhere)
+def MakeSquare():
+    from netgen.geom2d import SplineGeometry
+    square = SplineGeometry()
+    square.AddRectangle([-2,-2],[2,2],bc=1)
+    return square;
 
-# unit cube [-1,1]^3 (boundary index 1 everywhere)
-from netgen.csg import CSGeometry, OrthoBrick, Pnt
-cube = CSGeometry()
-cube.Add (OrthoBrick(Pnt(-2,-2,-2), Pnt(2,2,2)))
+# unit cube [-2,2]^3 (boundary index 1 everywhere)
+def MakeCube():
+    from netgen.csg import CSGeometry, OrthoBrick, Pnt
+    cube = CSGeometry()
+    cube.Add (OrthoBrick(Pnt(-2,-2,-2), Pnt(2,2,2)))
+    return cube;
 
 # meshing
 from netgen.meshing import MeshingParameters
 ngsglobals.msg_level = 1
-mesh = Mesh (cube.GenerateMesh(maxh=0.5, quad_dominated=False))
+mesh = Mesh (MakeCube().GenerateMesh(maxh=0.5, quad_dominated=False))
 
 # The mesh deformation calculator
 from deform import *
@@ -33,6 +37,7 @@ levelset = sqrt(x*x+y*y) - 1
 
 # Calculation of the deformation:
 deformation = lsetmeshadap.CalcDeformation(levelset)
+print ("Computed mesh transformation.")
 lset_ho = lsetmeshadap.lset_ho
 # lsetmeshadap.lset_p1
 print ("Max. dist of discr. intf. to exact level set fct:", lsetmeshadap.CalcMaxDistance(levelset))
@@ -41,10 +46,17 @@ print ("Max. dist of discr. intf. to discr. level set f.:", lsetmeshadap.CalcMax
 # Applying the mesh deformation
 mesh.SetDeformation(deformation)
 
+Vh = H1(mesh, order=3, dirichlet=[1])
+Vh_tr = CastToXFESpace( FESpace ("xfespace", mesh=mesh, order=3, 
+                                       flags = {"trace" : True, "dgjumps" : False, "ref_space" : 0}))
+Vh_tr.SetBaseFESpace(Vh)
+Vh_tr.SetLevelSet(levelset)
+Vh_tr.Update()
 
+print ("Trace FE Space created.")
 #unset the mesh deformation
 mesh.UnsetDeformation() 
 mesh.Refine()
 
 
-print ("ran through")
+print ("ran through.")
