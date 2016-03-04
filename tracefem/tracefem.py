0725cd252c51f5624552f08f50e3251d527a4e90
Christoph Lehrenfeld <christoph.lehrenfeld@uni-muenster.de>
2016-03-04 13:50:19 +0100

    [python] export tracefem symbols separately...

--- tracefem/tracefem.py
+++ tracefem/tracefem.py
@@ -1,6 +1,7 @@
 from ngsolve.comp import *
 from ngsolve.fem import *
 from libngsxfem_py.xfem import *
+import libngsxfem_tracefem
 
 def TraceFESpace(mesh, stdfes=None, levelset=None, dgjumps=False, ref_space=0 ):
     Vh_tr = CastToXFESpace (FESpace ("xfespace", mesh=mesh, flags = {"trace" : True, "dgjumps" : dgjumps, "ref_space" : ref_space}) )
---
+++ tracefem/tracefem_python.cpp
@@ -0,0 +1,5 @@
+#include <python_ngstd.hpp>
+BOOST_PYTHON_MODULE(libngsxfem_tracefem) 
+{
+  std::cout << "loading tracefem stuff" << std::endl;
+}
