325ea85c7da7f202ee93b5ed721aeec31ed4c088
Christoph Lehrenfeld <christoph.lehrenfeld@rwth-aachen.de>
2016-03-26 11:12:49 +0100

    [stokes] postpone xstokes-fespace update if nolset

--- stokes/xstokesspace.hpp
+++ stokes/xstokesspace.hpp
@@ -15,21 +15,38 @@ namespace ngcomp
 
   class XStokesFESpace : public CompoundFESpace
   {
+    shared_ptr<CoefficientFunction> lset = nullptr;
   public:
     XStokesFESpace (shared_ptr<MeshAccess> ama,
                     const Flags & flags);
     virtual ~XStokesFESpace () { ; }
     virtual string GetClassName () const { return "XStokesFESpace"; }
 
-    void SetLevelSet(shared_ptr<CoefficientFunction> lset){ 
+    void SetLevelSet(shared_ptr<CoefficientFunction> alset){
+      lset = alset;
       for (int i = 0; i < spaces.Size(); ++i)
-        dynamic_pointer_cast<XStdFESpace>(spaces[i]) -> SetLevelSet(lset);
+        dynamic_pointer_cast<XStdFESpace>(spaces[i]) -> SetLevelSet(alset);
     };
 
     void SetLevelSet(shared_ptr<GridFunction> lset){ 
       SetLevelSet(make_shared<GridFunctionCoefficientFunction>(lset));
     };
 
+    virtual void Update(LocalHeap & lh)
+    {
+      if ( lset == nullptr )
+        cout << "xstokesspace: no lset, Update postponed " << endl;
+      else
+        CompoundFESpace::Update(lh);
+    }    
+    
+    virtual void FinalizeUpdate(LocalHeap & lh)
+    {
+      if ( lset == nullptr )
+        cout << "xstokesspace: no lset, Update postponed " << endl;
+      else
+        CompoundFESpace::FinalizeUpdate(lh);
+    }    
     
   };
 
