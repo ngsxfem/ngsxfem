fb7106f9b6ac33fb3b685c6a8071808f4eb02db1
Christoph Lehrenfeld <christoph.lehrenfeld@rwth-aachen.de>
2016-03-26 11:14:09 +0100

    [stokes] xfem.stokes module to python

---
+++ stokes/stokes.py
@@ -0,0 +1,28 @@
+from ngsolve.comp import *
+from ngsolve.fem import *
+from libngsxfem_py.xfem import *
+
+import libngsxfem_xstokes
+
+def XStokesFESpace(mesh, order=1, levelset=None, dgjumps=False, dirichlet =[], ref_space=0 ):
+    Vh = CastToXStokesFESpace ( FESpace ("xstokes", mesh=mesh, dirichlet=dirichlet, flags = {"order" : order, "dgjumps" : dgjumps, "ref_space" : ref_space}) )
+    if (levelset!=None):
+        Vh.SetLevelSet(levelset)
+    Vh.Update()
+    print ("StokesFESpace-Update: done")
+    return Vh
+
+def TwoDomainStokesIntegrator (muneg, mupos):
+    return BFI("xstokes", coef=[muneg,mupos])
+
+
+def NitscheStokesBLFIntegrator (muneg, mupos, lamb=10):
+    return BFI("xstokesnitsche", coef=[muneg,mupos,lamb])
+
+def NitscheStokesLFIntegrator (gammaf):
+    return LFI("xstokesnitscherhs", coef=[gammaf])
+
+def NitscheStokesIntegrators ( muneg, mupos, lamb=10, gammaf=0 ):
+    return (BFI("xstokesnitsche", coef=[muneg,mupos,lamb]), LFI("xstokesnitscherhs", coef=gammaf))
+
+
