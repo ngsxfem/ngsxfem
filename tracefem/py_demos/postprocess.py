2da67468080297f64f1e9f186888f3ff6a587fd3
Christoph Lehrenfeld <christoph.lehrenfeld@uni-muenster.de>
2016-04-05 18:47:43 +0200

    [tracefem] postprocess script... other things now..

--- tracefem/py_demos/postprocess.py
+++ tracefem/py_demos/postprocess.py
@@ -13,7 +13,7 @@ for order in orders:
 
 for order in orders:
     print("\\midrule")
-    for i in range(4):
+    for i in range(3):
         if (i==0):
             pbegin = "{}".format(order)
         else:
@@ -27,18 +27,21 @@ for order in orders:
                     eoc = "{:>0.1f}".format(log(resultdict[(i-1,order)][errortype] / resultdict[(i,order)][errortype])/log(2.0))
                 eoc = "{:>4}".format(eoc)
                 print("& \\num{{{:0.5e}}} & ({}) ".format(resultdict[(i,order)][errortype],eoc),end="")
+            for othertype in ["numits"]:
+                print("& {} ".format(resultdict[(i,order)][othertype]),end="")
+            
         print(" \\\\")
 print("\\bottomrule")
 
 
 
-for order in orders:
-    print("\\midrule")
-    i = 3
-    pbegin = "{}".format(order)
-    print("{} ".format(pbegin),end="")
-    if (i,order) in resultdict:
-        for errortype in ["total_ndofs","global_ndofs","dg_ndofs","nze","dg_nze"]:
-            print("& {:9.0f} K ".format(resultdict[(i,order)][errortype]/1000),end="")
-        print(" \\\\")
-print("\\bottomrule")
+# for order in orders:
+#     print("\\midrule")
+#     i = 3
+#     pbegin = "{}".format(order)
+#     print("{} ".format(pbegin),end="")
+#     if (i,order) in resultdict:
+#         for errortype in ["total_ndofs","global_ndofs","dg_ndofs","nze","dg_nze"]:
+#             print("& {:9.0f} K ".format(resultdict[(i,order)][errortype]/1000),end="")
+#         print(" \\\\")
+# print("\\bottomrule")
