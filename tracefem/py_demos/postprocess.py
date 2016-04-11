e9dcf2b912637f6a66c2c0476b0008194e67a2b0
Christoph Lehrenfeld <christoph.lehrenfeld@uni-muenster.de>
2016-04-11 15:22:49 +0200

    [tracefem,hdg] postprocess from..

--- tracefem/py_demos/postprocess.py
+++ tracefem/py_demos/postprocess.py
@@ -1,6 +1,15 @@
 from math import log
 import pickle
-with open('results.pkl', 'rb') as f:
+
+
+import argparse        
+
+parser = argparse.ArgumentParser(description='Postprocess laplace beltrami results')
+parser.add_argument('--from', dest="former_results", nargs='?', default="results.pkl", help='reuse old results from file')
+args = parser.parse_args()
+options = vars(args)
+
+with open(options["former_results"], 'rb') as f:
     resultdict = pickle.load(f)
 
 orders = [1,2,3,4,5,6,7,8,9,10]
@@ -36,9 +45,12 @@ for order in orders:
                     eoc = "{:>0.1f}".format(log(resultdict[(i-1,order)][errortype] / resultdict[(i,order)][errortype])/log(2.0))
                 eoc = "{:>4}".format(eoc)
                 print("& \\num{{{:0.5e}}} & ({}) ".format(resultdict[(i,order)][errortype],eoc),end="")
-            for othertype in ["global_ndofs","total_ndofs","nze","dg_ndofs","dg_nze","cg_ndofs","cg_nze","cg_gp_nze"]:
+            for othertype in ["global_ndofs","total_ndofs","dg_ndofs","cg_ndofs"]:
                 if othertype in resultdict[(i,order)]:
-                    print("& {:10}".format(resultdict[(i,order)][othertype]),end="")
+                    print("& {:8}".format(resultdict[(i,order)][othertype]),end="")
+            for othertype in ["nze","dg_nze","cg_gp_nze","cg_nze"]:
+                if othertype in resultdict[(i,order)]:
+                    print("& {:6}".format(int(resultdict[(i,order)][othertype]/1000)),end="")
             print(" \\\\")
 print("\\bottomrule")
 
