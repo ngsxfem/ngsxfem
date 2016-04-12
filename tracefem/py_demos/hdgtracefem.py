242563cf26ad08f267a9e464bc71b90b62806f3f
Christoph Lehrenfeld <christoph.lehrenfeld@uni-muenster.de>
2016-04-12 17:26:58 +0200

    [tracefem,hdg] py_demo: convection problem...

--- tracefem/py_demos/hdgtracefem.py
+++ tracefem/py_demos/hdgtracefem.py
@@ -26,7 +26,7 @@ ngsglobals.numthreads=16
 
 h = specialcf.mesh_size
 # 3D: circle configuration
-def Make3DProblem():
+def Make3DProblem_Diffusion():
     from netgen.csg import CSGeometry, OrthoBrick, Pnt
     cube = CSGeometry()
     cube.Add (OrthoBrick(Pnt(-1.41,-1.41,-1.41), Pnt(1.41,1.41,1.41)))
@@ -57,6 +57,43 @@ def Make3DProblem():
                "checkCG_GP_pattern" : True
     }
     return problem;
+
+def Make3DProblem_Convection():
+    from netgen.csg import CSGeometry, OrthoBrick, Pnt
+    cube = CSGeometry()
+    cube.Add (OrthoBrick(Pnt(-1.41,-1.41,-1.41), Pnt(1.41,1.41,1.41)))
+    # mesh = Mesh (cube.GenerateMesh(maxh=0.5, quad_dominated=False))
+    mesh = Mesh (cube.GenerateMesh(maxh=1, quad_dominated=False))
+    mesh.Refine()
+
+    eps = 0.025
+    c = 1
+    problem = {"Diffusion" : 0.0,
+               "Convection" : CoefficientFunction((-y*sqrt(1-z*z),x*sqrt(1-z*z),0)),
+               "Reaction" : c,
+               # "Source" : (12 * eps*sqrt(eps)*x*y*z/(eps+4*y*y) + 16 *eps*sqrt(eps)*(1-z*z)*x*y*z/((eps+4*z*z)*(eps+4*z*z)) + (6*eps*x*y+sqrt(x*x+y*y)*(x*x-y*y)+x*y)*atan(2*z/sqrt(eps))),
+               "Source" : (sqrt(x*x+y*y)*(x*x-y*y)+x*y)*atan(2*z/sqrt(eps)) ,
+               "Solution" : x*y*atan(2*z/sqrt(eps)),
+               "GradSolution" : None,
+               "VolumeStabilization" : 1+eps/h+c*h,
+               "Levelset" : sqrt(x*x+y*y+z*z)-1,
+               "GradLevelset" : CoefficientFunction((x,y,z)),
+               "Lambda" : 10,
+               "Iterative" : False,
+               "Order" : 2,
+               "Mesh" : mesh,
+               "StaticCondensation" : True,
+               "HDG": True,
+               "checkDGpattern" : False,
+               "checkCGpattern" : False,
+               "checkCG_GP_pattern" : False
+    }
+    return problem;
+
+
+def Make3DProblem():
+    # return Make3DProblem_Diffusion()
+    return Make3DProblem_Convection()
 
 class Discretization(object):
     """
