bf1142288b272975419450f0275c263d1ddf73a7
Christoph Lehrenfeld <christoph.lehrenfeld@uni-muenster.de>
2016-04-12 17:24:00 +0200

    [tracefem,hdg] py_demo,postprocess: fix division by zero

--- tracefem/py_demos/postprocess.py
+++ tracefem/py_demos/postprocess.py
@@ -39,12 +39,16 @@ for order in orders:
         print("{} ".format(pbegin),end="")
         if (i,order) in resultdict:
             for errortype in ["maxdist","l2err","h1normerr","h1tangerr"]:
-                if (i==0):
-                    eoc = " ---"
-                else:
-                    eoc = "{:>0.1f}".format(log(resultdict[(i-1,order)][errortype] / resultdict[(i,order)][errortype])/log(2.0))
-                eoc = "{:>4}".format(eoc)
-                print("& \\num{{{:0.5e}}} & ({}) ".format(resultdict[(i,order)][errortype],eoc),end="")
+                if errortype in resultdict[(i,order)]:
+                    if (i==0):
+                        eoc = " ---"
+                    else:
+                        try:
+                            eoc = "{:>0.1f}".format(log(resultdict[(i-1,order)][errortype] / resultdict[(i,order)][errortype])/log(2.0))
+                        except:
+                            eoc = " ---"
+                    eoc = "{:>4}".format(eoc)
+                    print("& \\num{{{:0.5e}}} & ({}) ".format(resultdict[(i,order)][errortype],eoc),end="")
             # for othertype in ["global_ndofs","total_ndofs","dg_ndofs","cg_ndofs","cg_global_ndofs"]:
             #     if othertype in resultdict[(i,order)]:
             #         print("& {:8}".format(resultdict[(i,order)][othertype]),end="")
