a7b771164217dbc02dc0295f1655d11a99eb41d2
Christoph Lehrenfeld <christoph.lehrenfeld@rwth-aachen.de>
2016-04-24 23:03:55 +0200

    [py,nitsche] python bindings for nitsche+xlaplace

--- xfem/basics.py
+++ xfem/basics.py
@@ -89,6 +89,8 @@ def XFESpace(basefes,levelset=None,flags={},empty=None,ref_space=None):
         print("no levelset given, update postponed")
     return fes
         
+def TwoDomainLaplaceIntegrator (coefneg,coefpos):
+    return BFI("xlaplace", coef=[coefneg,coefpos])
 
 def TwoDomainMassIntegrator (coefneg,coefpos):
     return BFI("xmass", coef=[coefneg,coefpos])
@@ -96,7 +98,126 @@ def TwoDomainMassIntegrator (coefneg,coefpos):
 def TwoDomainSourceIntegrator (coefneg,coefpos):
     return LFI("xsource", coef=[coefneg,coefpos])
 
+def XNitscheIntegrators (diffusion, henryweights=[1.0,1.0], weighting="hansbo", stab_param=10.0, minstab=False,fluxjump=None,jump=None):
+    """
+    Returns Integrators for (scalar) Nitsche-XFEM discretiz.,
+    i.e. weak enforcement of interface conditions in XFEM-setting
+    interface conditions are:
+    [u] = g 
+    and [a du/dn] = h with a the piecewise constant diffusion
+    =============================================================
+    Integrators for (scalar) Nitsche-XFEM discretizations    
+    returns a 3-tuple of Integrators: 
+      1. BFI: NitscheXFEM - left hand side
+      2. LFI: NitscheXFEM - r.h.s. w.r.t. jump g in [u] = g
+      3. LFI: NitscheXFEM - r.h.s. w.r.t. jump h in [a du/dn] = h
+    =============================================================
+    necessary arguments:
+    - diffusion : 
+        list of diffusion constants
+    - henryweights : 
+        list of henry weights (default = [1,1])
+    - weighting : 
+        weighting of the averaging in  Nitsche form.
+        {u} = k_1 u_1 + k_2 * u_2
+        possibilities are (k_2 = 1.0 - k_1):
+         - "hansbo" (default): 
+            volume weighted average:
+              k_1 = |T_1| / |T|
+         - "halfhalf" / "naive": 
+            equal weights:
+              k_1 = k_2 = 0.5
+         - "heaviside": 
+              k_1 = 1.0 if |T_1| / |T| > 0.5
+              k_1 = 0.0 else
+        default is "hansbo"
+    - stab_param : 
+        penalty parameter in penalty of Nitsche form.
+        default is 10.0.
+    - minstab : 
+        Formulation with lifting stabilization. 
+        No stab_param needed in this formulation (value ignored).
+        default = False
+    - jump : 
+        coefficient g in interface condition [u] = h
+        default is None, i.e. [u] = 0
+    - fluxjump : 
+        coefficient h in interface condition [a du/dn] = h
+        default is None, i.e. [a du/dn] = 0
+    """
+    
+    rhs1 = None
+    rhs2 = None
+    
+    if minstab:
+        coefs=[diffusion[0], diffusion[1], henryweights[0], henryweights[1]]
+        if fluxjump != None:
+            coefs_fluxjump=[CoefficientFunction(henryweights[0]),
+                            CoefficientFunction(henryweights[1]),
+                            CoefficientFunction(fluxjump)]
+        if jump != None:
+            coefs_jump=[CoefficientFunction(diffusion[0]),
+                        CoefficientFunction(diffusion[1]),
+                        CoefficientFunction(henryweights[0]),
+                        CoefficientFunction(henryweights[1]),
+                        CoefficientFunction(jump)]
+
+        if weighting.lower() == "hansbo":
+            lhs = BFI("xnitsche_minstab_hansbo", coef=coefs)
+            if jump != None:
+                rhs1 = LFI("xnitscherhsjump_hansbo", coef=coefs_jump)
+            if fluxjump != None:
+                rhs2 = LFI("xnitscherhsfluxjump_hansbo", coef=coefs_fluxjump)
+        elif weighting.lower() == "halfhalf" or weighting.lower() == "naive":
+            lhs = BFI("xnitsche_minstab_halfhalf", coef=coefs)
+            if jump != None:
+                rhs1 = LFI("xnitscherhsjump_halfhalf", coef=coefs_jump)
+            if fluxjump != None:
+                rhs2 = LFI("xnitscherhsfluxjump_halfhalf", coef=coefs_fluxjump)
         elif weighting.lower() == "heaviside":
+            lhs = BFI("xnitsche_minstab_heaviside", coef=coefs)
+            if jump != None:
+                rhs1 = LFI("xnitscherhsjump_heaviside", coef=coefs_jump)
+            if fluxjump != None:
+                rhs2 = LFI("xnitscherhsfluxjump_heaviside", coef=coefs_fluxjump)
+        else:
+            print ("unknown weighting for nitsche integrators!")
+    else:
+        coefs=[diffusion[0], diffusion[1], henryweights[0], henryweights[1], stab_param]
+        if fluxjump != None:
+            coefs_fluxjump=[CoefficientFunction(henryweights[0]),
+                            CoefficientFunction(henryweights[1]),
+                            CoefficientFunction(fluxjump)]
+        if jump != None:
+            coefs_jump=[CoefficientFunction(diffusion[0]),
+                        CoefficientFunction(diffusion[1]),
+                        CoefficientFunction(henryweights[0]),
+                        CoefficientFunction(henryweights[1]),
+                        CoefficientFunction(jump),
+                        CoefficientFunction(stab_param)]
+            
+        if weighting.lower() == "hansbo":
+            lhs = BFI("xnitsche_hansbo", coef=coefs)
+            if jump != None:
+                rhs1 = LFI("xnitscherhsjump_hansbo", coef=coefs_jump)
+            if fluxjump != None:
+                rhs2 = LFI("xnitscherhsfluxjump_hansbo", coef=coefs_fluxjump)
+        elif weighting.lower() == "halfhalf" or weighting.lower() == "naive":
+            lhs = BFI("xnitsche_halfhalf", coef=coefs)
+            if jump != None:
+                rhs1 = LFI("xnitscherhsjump_halfhalf", coef=coefs_jump)
+            if fluxjump != None:
+                rhs2 = LFI("xnitscherhsfluxjump_halfhalf", coef=coefs_fluxjump)
+        elif weighting.lower() == "heaviside":
+            lhs = BFI("xnitsche_heaviside", coef=coefs)
+            if jump != None:
+                rhs1 = LFI("xnitscherhsjump_heaviside", coef=coefs_jump)
+            if fluxjump != None:
+                rhs2 = LFI("xnitscherhsfluxjump_heaviside", coef=coefs_fluxjump)
+        else:
+            print ("unknown weighting for nitsche integrators!")
+        
+    return (lhs,rhs1,rhs2)
 
 
 negative_domain = dict()
