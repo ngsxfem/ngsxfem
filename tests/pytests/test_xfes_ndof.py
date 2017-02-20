ee840d3e752cb72cbb8b215d28aff0c0d363db4b
Christoph Lehrenfeld <christoph.lehrenfeld@rwth-aachen.de>
2017-02-20 16:58:02 +0100

    [pytest] structured grid in ndof test

--- tests/pytests/test_xfes_ndof.py
+++ tests/pytests/test_xfes_ndof.py
@@ -4,19 +4,23 @@ from netgen.geom2d import unit_square
 from netgen.csg import unit_cube
 
 def test_xfes_ndof_2D(quad=False):
-    mesh = Mesh (unit_square.GenerateMesh(maxh=0.2,quad_dominated=quad))
+    mesh = Mesh (unit_square.GenerateMesh(maxh=2,quad_dominated=quad))
+    mesh.Refine()
+    mesh.Refine()
     lsetp1 = GridFunction(H1(mesh,order=1))
     InterpolateToP1((sqrt(x*x+y*y) - 1.0/3.0),lsetp1)
     Vh = H1(mesh, order=1, dirichlet=[1,2,3,4])
     Vhx = XFESpace(Vh, mesh, lsetp1)
-    assert Vh.ndof == 43
-    assert Vhx.ndof == 7
+    assert Vh.ndof == 65
+    assert Vhx.ndof == 10
 
 def test_xfes_ndof_3D(quad=False):
-    mesh = Mesh (unit_cube.GenerateMesh(maxh=0.2,quad_dominated=quad))
+    mesh = Mesh (unit_cube.GenerateMesh(maxh=2,quad_dominated=quad))
+    mesh.Refine()
+    mesh.Refine()
     lsetp1 = GridFunction(H1(mesh,order=1))
     InterpolateToP1((sqrt(x*x+y*y) - 1.0/3.0),lsetp1)
     Vh = H1(mesh, order=1, dirichlet=[1,2,3,4])
     Vhx = XFESpace(Vh, mesh, lsetp1)
-    assert Vh.ndof == 228
-    assert Vhx.ndof == 47
+    assert Vh.ndof == 125
+    assert Vhx.ndof == 33
