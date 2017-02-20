0bc7be7e823e377bb958de9ccdafdd43fd752bbe
Christoph Lehrenfeld <christoph.lehrenfeld@rwth-aachen.de>
2017-02-20 17:50:46 +0100

    use meshes for reference values

--- tests/pytests/test_xfes_ndof.py
+++ tests/pytests/test_xfes_ndof.py
@@ -3,21 +3,17 @@ from xfem import *
 from netgen.geom2d import unit_square
 from netgen.csg import unit_cube
 
-def test_xfes_ndof_2D(quad=False):
-    mesh = Mesh (unit_square.GenerateMesh(maxh=2,quad_dominated=quad))
-    mesh.Refine()
-    mesh.Refine()
+def test_xfes_ndof_2D():
+    mesh = Mesh("mesh2D.vol.gz")
     lsetp1 = GridFunction(H1(mesh,order=1))
     InterpolateToP1((sqrt(x*x+y*y) - 1.0/3.0),lsetp1)
     Vh = H1(mesh, order=1, dirichlet=[1,2,3,4])
     Vhx = XFESpace(Vh, mesh, lsetp1)
-    assert Vh.ndof == 65
-    assert Vhx.ndof == 10
+    assert Vh.ndof == 25
+    assert Vhx.ndof == 8
 
-def test_xfes_ndof_3D(quad=False):
-    mesh = Mesh (unit_cube.GenerateMesh(maxh=2,quad_dominated=quad))
-    mesh.Refine()
-    mesh.Refine()
+def test_xfes_ndof_3D():
+    mesh = Mesh("mesh3D.vol.gz")
     lsetp1 = GridFunction(H1(mesh,order=1))
     InterpolateToP1((sqrt(x*x+y*y) - 1.0/3.0),lsetp1)
     Vh = H1(mesh, order=1, dirichlet=[1,2,3,4])
