83297edbaa77128641cd8f918f2f716c2d93f403
Christoph Lehrenfeld <christoph.lehrenfeld@rwth-aachen.de>
2017-02-20 14:30:36 +0100

    [tests] remove zero level at vertices in test for now (see issue #12)

--- tests/pytests/test_xfes_ndof.py
+++ tests/pytests/test_xfes_ndof.py
@@ -6,17 +6,17 @@ from netgen.csg import unit_cube
 def test_xfes_ndof_2D(quad=False):
     mesh = Mesh (unit_square.GenerateMesh(maxh=0.2,quad_dominated=quad))
     lsetp1 = GridFunction(H1(mesh,order=1))
-    InterpolateToP1((sqrt(sqrt(x*x+y*y)) - 1.0),lsetp1)
+    InterpolateToP1((sqrt(x*x+y*y) - 1.0/3.0),lsetp1)
     Vh = H1(mesh, order=1, dirichlet=[1,2,3,4])
     Vhx = XFESpace(Vh, mesh, lsetp1)
     assert Vh.ndof == 43
-    assert Vhx.ndof == 21
+    assert Vhx.ndof == 7
 
 def test_xfes_ndof_3D(quad=False):
     mesh = Mesh (unit_cube.GenerateMesh(maxh=0.2,quad_dominated=quad))
     lsetp1 = GridFunction(H1(mesh,order=1))
-    InterpolateToP1((sqrt(sqrt(x*x+y*y)) - 1.0),lsetp1)
+    InterpolateToP1((sqrt(x*x+y*y) - 1.0/3.0),lsetp1)
     Vh = H1(mesh, order=1, dirichlet=[1,2,3,4])
     Vhx = XFESpace(Vh, mesh, lsetp1)
     assert Vh.ndof == 228
-    assert Vhx.ndof == 111
+    assert Vhx.ndof == 47
