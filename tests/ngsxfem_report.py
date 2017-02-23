fc49390489c32a294693d4bd8351ead8b196ab3c
Christoph Lehrenfeld <christoph.lehrenfeld@rwth-aachen.de>
2017-02-23 16:00:38 +0100

    [ngsxfem report] include Update()-timings

--- tests/ngsxfem_report.py
+++ tests/ngsxfem_report.py
@@ -3,12 +3,21 @@ from xfem import *
 from netgen.geom2d import unit_square
 from netgen.csg import unit_cube
 
+import time                                                
+
 ngsglobals.msg_level = 1
 
 def test_fes_timing_2D(quad=False):
     mesh = Mesh (unit_square.GenerateMesh(maxh=0.2,quad_dominated=quad))
     Vh = H1(mesh, order=1, dirichlet=[1,2,3,4])
-    return Vh.__timing__()
+    container = Vh.__timing__()
+    ts = time.time()
+    steps = 10
+    for i in range(steps):
+        Vh.Update()
+    te = time.time()
+    container.append(("Update",1e9*(te-ts)/steps))
+    return container
 
 def test_xfes_timing_2D(quad=False):
     mesh = Mesh (unit_square.GenerateMesh(maxh=0.2,quad_dominated=quad))
@@ -16,12 +25,26 @@ def test_xfes_timing_2D(quad=False):
     InterpolateToP1((sqrt(sqrt(x*x+y*y)) - 1.0),lsetp1)
     Vh = H1(mesh, order=1, dirichlet=[1,2,3,4])
     Vhx = XFESpace(Vh, lsetp1)
-    return Vhx.__timing__()
+    container = Vhx.__timing__()
+    ts = time.time()
+    steps = 10
+    for i in range(steps):
+        Vhx.Update()
+    te = time.time()
+    container.append(("Update",1e9*(te-ts)/steps))
+    return container
 
 def test_fes_timing_3D(quad=False):
     mesh = Mesh (unit_cube.GenerateMesh(maxh=0.2,quad_dominated=quad))
     Vh = H1(mesh, order=1, dirichlet=[1,2,3,4])
-    return Vh.__timing__()
+    container = Vh.__timing__()
+    ts = time.time()
+    steps = 10
+    for i in range(steps):
+        Vh.Update()
+    te = time.time()
+    container.append(("Update",1e9*(te-ts)/steps))
+    return container
 
 def test_xfes_timing_3D(quad=False):
     mesh = Mesh (unit_cube.GenerateMesh(maxh=0.2,quad_dominated=quad))
@@ -29,7 +52,14 @@ def test_xfes_timing_3D(quad=False):
     InterpolateToP1((sqrt(sqrt(x*x+y*y)) - 1.0),lsetp1)
     Vh = H1(mesh, order=1, dirichlet=[1,2,3,4])
     Vhx = XFESpace(Vh, lsetp1)
-    return Vhx.__timing__()
+    container = Vhx.__timing__()
+    ts = time.time()
+    steps = 10
+    for i in range(steps):
+        Vhx.Update()
+    te = time.time()
+    container.append(("Update",1e9*(te-ts)/steps))
+    return container
 
 print("\nCalculations: \n\n")
 stiming2d = test_fes_timing_2D()
