bf5ba96b79238cb65f68ae86ecf97ac78efcf164
Fabian Heimann <Fabian.Heimann@stud.uni-goettingen.de>
2017-02-25 17:09:48 +0100

    Parametrizing the test III

--- tests/pytests/test_straightcutrule.py
+++ tests/pytests/test_straightcutrule.py
@@ -6,25 +6,19 @@ from math import pi
 
 @pytest.mark.parametrize("quad_dominated", [True, False])
 @pytest.mark.parametrize("order", [2,4,8])
+@pytest.mark.parametrize("domain", [NEG, POS, IF])
 
-def test_new_integrateX_via_circle_geom(quad_dominated, order):
+def test_new_integrateX_via_circle_geom(quad_dominated, order, domain):
     square = SplineGeometry()
     square.AddRectangle([0,0],[1,1],bc=1)
     mesh = Mesh (square.GenerateMesh(maxh=100, quad_dominated=quad_dominated))
     r=0.6
 
-    domains = [NEG, POS, IF]
-
     levelset = sqrt(x*x+y*y)-r
     referencevals = { POS : 1-pi*r*r/4, NEG : pi*r*r/4, IF : r*pi/2}
 
     n_ref = 8
-    errors = dict()
-    eoc = dict()
-
-    for key in domains:
-        errors[key] = []
-        eoc[key] = []
+    errors = []
 
     for i in range(n_ref):
         V = H1(mesh,order=1)
@@ -33,21 +27,17 @@ def test_new_integrateX_via_circle_geom(quad_dominated, order):
     
         f = CoefficientFunction(1)
     
-        for key in domains:
-            integral = NewIntegrateX(lset=lset_approx,mesh=mesh,cf=f,order=order,domain_type=key,heapsize=1000000, use_saye = True)
-            print("Result of Integration Reflevel ",i,", Key ",key," : ", integral)
-            errors[key].append(abs(integral - referencevals[key]))
+        integral = NewIntegrateX(lset=lset_approx,mesh=mesh,cf=f,order=order,domain_type=domain,heapsize=1000000, use_saye = True)            print("Result of Integration Reflevel ",i,", Key ",domain," : ", integral)
+        errors.append(abs(integral - referencevals[domain]))
 
         if i < n_ref - 1:
             mesh.Refine()
         
-    for key in domains:
-        eoc[key] = [log(errors[key][i+1]/errors[key][i])/log(0.5) for i in range(n_ref-1)]
+    eoc = [log(errors[i+1]/errors[i])/log(0.5) for i in range(n_ref-1)]
 
     print("L2-errors:", errors)
     print("experimental order of convergence (L2):", eoc)
 
-    for key in domains:
-        mean_eoc_array = eoc[key][1:]
-        mean_eoc = sum(mean_eoc_array)/len(mean_eoc_array)
-        assert mean_eoc > 1.75
+    mean_eoc_array = eoc[1:]
+    mean_eoc = sum(mean_eoc_array)/len(mean_eoc_array)
+    assert mean_eoc > 1.75
