8de84f794534f5a805ef831ba1cfc6c54c8ec825
Christoph Lehrenfeld <christoph.lehrenfeld@rwth-aachen.de>
2017-03-01 19:49:13 +0100

    one-file-per-key try

--- tests/ngsxfem_report.py
+++ tests/ngsxfem_report.py
@@ -2,14 +2,28 @@ from ngsolve import *
 from xfem import *
 from netgen.geom2d import unit_square
 from netgen.csg import unit_cube
+from sys import argv
 
 import time
 
 ngsglobals.msg_level = 1
 
-def test_fes_timing_2D(quad=False, order=1):
-    mesh = Mesh (unit_square.GenerateMesh(maxh=0.2,quad_dominated=quad))
-    Vh = H1(mesh, order=order, dirichlet=[1,2,3,4])
+def test_fes_timing(dimension=2,stdfes=True,quad=False, order=1):
+    if dimension == 2:
+        mesh = Mesh (unit_square.GenerateMesh(maxh=0.2,quad_dominated=quad))
+    else:
+        mesh = Mesh (unit_cube.GenerateMesh(maxh=0.2,quad_dominated=quad))
+
+    Vhs = H1(mesh, order=order, dirichlet=[1,2,3,4])
+
+    lsetp1 = GridFunction(H1(mesh,order=1))
+    InterpolateToP1((sqrt(sqrt(x*x+y*y)) - 1.0),lsetp1)
+
+    if stdfes:
+        Vh = Vhs
+    else:
+        Vhx = XFESpace(Vhs, lsetp1)
+        Vh = Vhx
     container = Vh.__timing__()
     ts = time.time()
     steps = 5
@@ -19,69 +33,31 @@ def test_fes_timing_2D(quad=False, order=1):
     container.append(("Update",1e9*(te-ts)/steps))
     return container
 
-def test_xfes_timing_2D(quad=False, order=1):
-    mesh = Mesh (unit_square.GenerateMesh(maxh=0.2,quad_dominated=quad))
-    lsetp1 = GridFunction(H1(mesh,order=1))
-    InterpolateToP1((sqrt(sqrt(x*x+y*y)) - 1.0),lsetp1)
-    Vh = H1(mesh, order=order, dirichlet=[1,2,3,4])
-    Vhx = XFESpace(Vh, lsetp1)
-    container = Vhx.__timing__()
-    ts = time.time()
-    steps = 5
-    for i in range(steps):
-        Vhx.Update()
-    te = time.time()
-    container.append(("Update",1e9*(te-ts)/steps))
-    return container
+## dd/mm/yyyy format
+date = time.strftime("%Y/%m/%d %H:%M:%S")
 
-def test_fes_timing_3D(quad=False, order=1):
-    mesh = Mesh (unit_cube.GenerateMesh(maxh=0.2,quad_dominated=quad))
-    Vh = H1(mesh, order=order, dirichlet=[1,2,3,4])
-    container = Vh.__timing__()
-    ts = time.time()
-    steps = 5
-    for i in range(steps):
-        Vh.Update()
-    te = time.time()
-    container.append(("Update",1e9*(te-ts)/steps))
-    return container
+if len(argv) > 1:
+    basedir = argv[1]
+else:
+    basedir = "./"
+if len(argv) > 2:
+    id = str(argv[2])
+else:
+    id = "0000"
 
-def test_xfes_timing_3D(quad=False, order=1):
-    mesh = Mesh (unit_cube.GenerateMesh(maxh=0.2,quad_dominated=quad))
-    lsetp1 = GridFunction(H1(mesh,order=1))
-    InterpolateToP1((sqrt(sqrt(x*x+y*y)) - 1.0),lsetp1)
-    Vh = H1(mesh, order=order, dirichlet=[1,2,3,4])
-    Vhx = XFESpace(Vh, lsetp1)
-    container = Vhx.__timing__()
-    ts = time.time()
-    steps = 5
-    for i in range(steps):
-        Vhx.Update()
-    te = time.time()
-    container.append(("Update",1e9*(te-ts)/steps))
-    return container
-
-print("\nCalculations: \n\n")
-stiming2d_k1 = test_fes_timing_2D()
-xtiming2d_k1 = test_xfes_timing_2D()
-stiming3d_k1 = test_fes_timing_3D()
-xtiming3d_k1 = test_xfes_timing_3D()
-stiming2d_k3 = test_fes_timing_2D(order=3)
-xtiming2d_k3 = test_xfes_timing_2D(order=3)
-stiming3d_k3 = test_fes_timing_3D(order=3)
-xtiming3d_k3 = test_xfes_timing_3D(order=3)
-with TaskManager():
-    stiming3d_k1_tm = test_fes_timing_3D()
-    xtiming3d_k1_tm = test_xfes_timing_3D()
-
-print("\n\nReport:")
-print(" FES Timings 2D - k = 1 :\n",stiming2d_k1)
-print("XFES Timings 2D - k = 1 :\n",xtiming2d_k1)
-print(" FES Timings 3D - k = 1 :\n",stiming3d_k1)
-print("XFES Timings 3D - k = 1 :\n",xtiming3d_k1)
-print(" FES Timings 3D - k = 1 with TaskManager :\n",stiming3d_k1_tm)
-print("XFES Timings 3D - k = 1 with TaskManager :\n",xtiming3d_k1_tm)
-print(" FES Timings 2D - k = 3 :\n",stiming2d_k3)
-print("XFES Timings 2D - k = 3 :\n",xtiming2d_k3)
-print(" FES Timings 3D - k = 3 :\n",stiming3d_k3)
-print("XFES Timings 3D - k = 3 :\n",xtiming3d_k3)
+filename1 = basedir + "fes_timings"
+for dim in [2,3]:
+    filename2 = filename1 + "_dim" + str(dim)
+    for stdfes in [True,False]:
+        if stdfes:
+            filename3 = filename2 + "_std"
+        else:
+            filename3 = filename2 + "_x"
+        for order in [1,3]:
+            filename4 = filename3 + "_order" + str(order)
+            report = test_fes_timing(dimension=dim,stdfes=stdfes,order=order)
+            for key, entry in report:
+                filename = filename4 + "_" + key.replace(" ", "_")
+                f = open(filename, 'a')
+                f.write("{:8}\t{:20}\t{:.6e}\n".format(id,date,entry))
+                f.close()
