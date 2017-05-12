e084f739b0d8d80ca186474603af58b06c8fdc50
Janosch Preuß <janosch.preuss@stud.uni-goettingen.de>
2017-05-12 13:56:00 +0200

    Corrections to variational formulation in test file

--- spacetime/fictdomain_Neumann.py
+++ spacetime/fictdomain_Neumann.py
@@ -26,11 +26,6 @@ tfe = ScalarTimeFE(k_t)
 
 st_fes = SpaceTimeFESpace(fes1,tfe)
 st_fes_ic = SpaceTimeFESpace(fes1,tfe)
-#print("k_t = {0}".format(st_fes.k_t()))
-#print("Nodes of TimeFE: {0}".format(st_fes.TimeFE_nodes().NumPy()))
-#visoptions.autoscale = False
-#visoptions.mminval=0.0
-#visoptions.mmaxval=1.0
 visoptions.deformation = 1
 
 #Fitted heat equation example
@@ -57,7 +52,7 @@ coeff_f = CoefficientFunction( pi*cos(pi*t)*sin(pi*x)*sin(pi*x)*sin(pi*y)*sin(pi
 
 u0 = GridFunction(st_fes)
 u0_ic = GridFunction(fes1)
-Draw(u0_ic)
+Draw(u0_ic,mesh,"u")
 u = st_fes.TrialFunction()
 v = st_fes.TestFunction()
 
@@ -68,23 +63,13 @@ f.Assemble()
 amat = a.mat.CreateMatrix()
 fvec = f.vec.CreateVector()
 
-#lset_neg = { "levelset" : lset_p1, "domain_type" : NEG, "subdivlvl" : 0}
+lset_neg = { "levelset" : lset_p1, "domain_type" : NEG, "subdivlvl" : 0}
 
 
 a = BilinearForm(st_fes,symmetric=False)
-#a += SymbolicBFI(levelset_domain = lset_neg, form = dt(u)*v, time_order=1)
-#a += SymbolicBFI(levelset_domain = lset_neg, form = delta_t*grad(u)*grad(v), time_order=1)
-#a += SymbolicBFI(form = fix_t(u,0)*fix_t(v,0) )
-
-a += SymbolicCutBFI(lset_p1, domain_type =  NEG, force_intorder = -1, 
-                    time_order = 1, subdivlvl = 0, 
-                    form = dt(u)*v )
-a += SymbolicCutBFI(lset_p1, domain_type =  NEG, force_intorder = -1, 
-                    time_order = 1, subdivlvl = 0, 
-                    form = delta_t*grad(u)*grad(v))
-a += SymbolicCutBFI(lset_p1, domain_type =  NEG, force_intorder = -1, 
-                    time_order = 1, subdivlvl = 0, 
-                    form = fix_t(u,0)*fix_t(v,0))
+a += SymbolicBFI(levelset_domain = lset_neg, form = dt(u)*v, time_order=2)
+a += SymbolicBFI(levelset_domain = lset_neg, form = delta_t*grad(u)*grad(v), time_order=2)
+a += SymbolicBFI(form = fix_t(u,0)*fix_t(v,0) )
 
 
 a.Assemble()
@@ -95,12 +80,8 @@ u0_ic.Set(u_exact)
 while tend - t_old > delta_t/2:
               
     f = LinearForm(st_fes)
-    #f += SymbolicLFI(levelset_domain = lset_neg, form = delta_t*coeff_f*v, time_order=1)
-    #f += SymbolicLFI(form = u0_ic*v )
-    f += SymbolicCutLFI(lset = lset_p1, domain_type =  NEG, force_intorder = -1, 
-                    time_order = 1, subdivlvl = 0,  form = delta_t*coeff_f*v)
-    f += SymbolicCutLFI(lset = lset_p1, domain_type =  NEG, force_intorder = -1, 
-                    time_order = 1, subdivlvl = 0,  form = u0_ic*v)
+    f += SymbolicLFI(levelset_domain = lset_neg, form = delta_t*coeff_f*v, time_order=2)
+    f += SymbolicLFI(form = u0_ic*v )
     f.Assemble()
 
     u0.vec.data = a.mat.Inverse(st_fes.FreeDofs(),"umfpack") * f.vec
@@ -110,6 +91,8 @@ while tend - t_old > delta_t/2:
     
     t_old = t_old + delta_t
     told.Set(t_old)
+    
+    #Draw(sqrt((u_exact-u0_ic)*(u_exact-u0_ic)),mesh,"error")
     
     l2error = sqrt (Integrate ( (u_exact-u0_ic)*(u_exact-u0_ic), mesh))
            
