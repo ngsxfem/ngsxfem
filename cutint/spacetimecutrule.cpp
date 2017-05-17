b8bb2cec58a164b5f2f4d7983ae7f2bfcfc45132
Fabian Heimann <Fabian.Heimann@stud.uni-goettingen.de>
2017-05-17 22:21:24 +0200

    Repairing the size mistake

--- cutint/spacetimecutrule.cpp
+++ cutint/spacetimecutrule.cpp
@@ -4,7 +4,7 @@
 namespace xintegration
 {
     vector<double> root_finding(auto li, ScalarFiniteElement<1>* fe_time, LocalHeap& lh, int subdivs=50, int bisection_iterations = 40){
-        if(li.Size() == 1){
+        if(li.Size() == 2){
             if((li[0] >= 0) != (li[1] >= 0)){
                 return {-li[0]/(li[1] - li[0]) };
             }
