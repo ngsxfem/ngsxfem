9dfff35c715283bc985e453b7a4d77ee049ac917
Fabian Heimann <Fabian.Heimann@stud.uni-goettingen.de>
2017-06-30 00:29:40 +0200

    New Mesh generation option

--- cutint/py_demos/draw_point.py
+++ cutint/py_demos/draw_point.py
@@ -19,18 +19,71 @@ def Make2DProblem(maxh,c = ([0,0],[1,1]), quad = False):
     mesh = Mesh (square.GenerateMesh(maxh=maxh, quad_dominated=quad))
     return mesh;
 
+def gen_mesh(n):
+    from netgen.meshing import Mesh, MeshPoint, Element3D, FaceDescriptor, Element2D
+    
+    mesh = Mesh()
+
+    pids = []
+    for i in range(n+1):
+        for j in range(n+1):
+            for k in range(n+1):
+                pids.append (mesh.Add (MeshPoint(Pnt(i/n, j/n, k/n))))
+                
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                base = i * (n+1)*(n+1) + j*(n+1) + k
+                baseup = base+(n+1)*(n+1)
+                pnum = [base,base+1,base+(n+1)+1,base+(n+1),
+                        baseup, baseup+1, baseup+(n+1)+1, baseup+(n+1)]
+                elpids = [pids[p] for p in pnum]
+                mesh.Add (Element3D(1,elpids))
+
+
+
+    def AddSurfEls (p1, dx, dy, facenr):
+        for i in range(n):
+            for j in range(n):
+                base = p1 + i*dx+j*dy
+                pnum = [base, base+dx, base+dx+dy, base+dy]
+                elpids = [pids[p] for p in pnum]
+                mesh.Add (Element2D(facenr,elpids))
+
+                            
+    mesh.Add (FaceDescriptor(surfnr=1,domin=1,bc=1))
+    AddSurfEls (0, 1, n+1, facenr=1)
+
+    mesh.Add (FaceDescriptor(surfnr=2,domin=1,bc=2))
+    AddSurfEls (0, (n+1)*(n+1), 1, facenr=2)
+
+    mesh.Add (FaceDescriptor(surfnr=3,domin=1,bc=3))
+    AddSurfEls (0, n+1, (n+1)*(n+1), facenr=3)
+
+
+    mesh.Add (FaceDescriptor(surfnr=4,domin=1,bc=4))
+    AddSurfEls ((n+1)**3-1, -(n+1), -1, facenr=1)
+
+    mesh.Add (FaceDescriptor(surfnr=5,domin=1,bc=5))
+    AddSurfEls ((n+1)**3-1, -(n+1)*(n+1), -(n+1), facenr=1)
+
+    mesh.Add (FaceDescriptor(surfnr=6,domin=1,bc=6))
+    AddSurfEls ((n+1)**3-1, -1, -(n+1)*(n+1), facenr=1)
+    return mesh    
+
+
 def Make3DProblem(maxh,c = (Pnt(0,0,0),Pnt(1,1,1)), quad = True):
     ngsglobals.msg_level = 0
 
     cube = OrthoBrick( c[0], c[1] ).bc(1)
     geom = CSGeometry()
     geom.Add (cube)
-    ngmesh = geom.GenerateMesh(maxh=maxh, quad_dominated=quad)
+    ngmesh = gen_mesh(1) # geom.GenerateMesh(maxh=maxh, quad_dominated=quad)
     mesh = Mesh(ngmesh)
     return mesh
 
 #levelset = sqrt(x*x+y*y)-0.6666666666 #.Compile()
-levelset = 1-2*x-2*y-2*z
+levelset = 1- 2*x-2*y- 2*z
 
 for order in range(0,3):
     call(["rm", "content_" + str(order) + ".tex"])
