43567cb91acf3cbd451a86409b6fe09b878605b9
Christoph Lehrenfeld <lehrenfeld@math.uni-goettingen.de>
2017-08-01 12:19:54 +0200

    shift to a fixed point iteration (more robust and not much more expensive)

--- lsetcurving/shiftedevaluate.cpp
+++ lsetcurving/shiftedevaluate.cpp
@@ -27,22 +27,10 @@ namespace ngfem
     auto elid = mip.GetTransformation().GetElementId();
     Array<int> dnums;
 
-    back->GetFESpace()->GetDofNrs(elid,dnums);
-    FlatVector<> values_back(dnums.Size()*DIM_SPACE,lh);
-    FlatMatrixFixWidth<2> vector_back(dnums.Size(),&(values_back(0)));
-    back->GetVector().GetIndirect(dnums,values_back);
-
     forth->GetFESpace()->GetDofNrs(elid,dnums);
     FlatVector<> values_forth(dnums.Size()*DIM_SPACE,lh);
     FlatMatrixFixWidth<2> vector_forth(dnums.Size(),&(values_forth(0)));
     forth->GetVector().GetIndirect(dnums,values_forth);
-
-    FiniteElement& fe_back = back->GetFESpace()->GetFE(elid,lh);
-    const ScalarFiniteElement<2> & scafe_back =
-            dynamic_cast<const ScalarFiniteElement<2> & > (fe_back);
-    FlatVector<> shape_back(dnums.Size(),lh);
-    scafe_back.CalcShape(ip,shape_back);
-    Vec<2> dvec_back = Trans(vector_back)*shape_back;
 
     FiniteElement& fe_forth = forth->GetFESpace()->GetFE(elid,lh);
     const ScalarFiniteElement<2> & scafe_forth =
@@ -53,19 +41,64 @@ namespace ngfem
 
     Vec<2> z = mip.GetPoint() + dvec_forth;
 
-    Vec<2> x = ip.Point();
+    // Solve the problem Theta(Phi(x)) = z
+
+    back->GetFESpace()->GetDofNrs(elid,dnums);
+    FlatVector<> values_back(dnums.Size()*DIM_SPACE,lh);
+    FlatMatrixFixWidth<2> vector_back(dnums.Size(),&(values_back(0)));
+    back->GetVector().GetIndirect(dnums,values_back);
+    
+    FiniteElement& fe_back = back->GetFESpace()->GetFE(elid,lh);
+    const ScalarFiniteElement<2> & scafe_back =
+            dynamic_cast<const ScalarFiniteElement<2> & > (fe_back);
+    FlatVector<> shape_back(dnums.Size(),lh);
+    scafe_back.CalcShape(ip,shape_back);
+    Vec<2> dvec_back = Trans(vector_back)*shape_back;
+    
+
     int its = 0;
     const double h = sqrt(mip.GetJacobiDet());
-    Vec<2> diff = mip.GetPoint() + dvec_back - z;
-    IntegrationPoint ipx(x);
-    while (its==0 || (L2Norm(diff) > 1e-8*h && its < 200))
+    Vec<2> diff;
+    IntegrationPoint ipx(ip);
+
+    IntegrationPoint ipx0(0,0,0);
+    MappedIntegrationPoint<2,2> mip_x0(ipx0,mip.GetTransformation());
+    Vec<2> zdiff = z-mip_x0.GetPoint();
+    
+    static atomic<int> cnt_its(0);
+    static atomic<int> cnt_calls(0);
+      
+    // Fixed point iteration
+    while (true)
+    {
+      scafe_back.CalcShape(ipx,shape_back);
+      dvec_back = Trans(vector_back)*shape_back;
+      diff = zdiff - dvec_back - mip.GetJacobian() * ipx.Point();
+      // cout << "diff = " << diff << endl;
+      // cout << "its = " << its << endl;
+      if ( L2Norm(diff) < 1e-8*h ) break;
+      ipx.Point() = mip.GetJacobianInverse() * (zdiff - dvec_back);
+
+      its++;
+      cnt_its++;
+      
+    }
+    cnt_calls++;
+    // cout << "cnt/calls = " << cnt_its/cnt_calls << endl;
+
+    /* 
+    FlatMatrixFixWidth<2> dshape_back(dnums.Size(),lh);
+    FlatVector<> shape_back_new(dnums.Size(),lh);
+
+    // not so robust Newton-type version (not working so well):
+    
+    while (its==0 || (L2Norm(diff) > 1e-8*h && its < 20000))
     {
       MappedIntegrationPoint<2,2> mip_x0(ipx,mip.GetTransformation());
       scafe_back.CalcShape(ipx,shape_back);
       dvec_back = Trans(vector_back)*shape_back;
       diff = z - dvec_back - mip_x0.GetPoint();
       Mat<2> dphi = mip_x0.GetJacobian();
-      FlatMatrixFixWidth<2> dshape_back(dnums.Size(),lh);
       scafe_back.CalcDShape(ipx,dshape_back);
       Mat<2> dback_ref = Trans(dshape_back)*vector_back;
       Mat<2> dback = Trans(mip_x0.GetJacobianInverse())*dback_ref + dphi;
@@ -87,15 +120,14 @@ namespace ngfem
       // Stuff to calculate function value at
       // prospective new iterate
       IntegrationPoint ipx_new(x);
-      FlatVector<> shape_back_new(dnums.Size(),lh);
       // the difference in the Armijo condition of which all components
       // need to be <= 0
       Vec<2> diffb;
 
-      while(itsb == 0 || ( ( (diffb(0) > 1e-8*h) || (diffb(1) > 1e-8*h) ) && itsb < 20) )
+      while(itsb == 0 || ( ( (diffb(0) > 1e-8*h) || (diffb(1) > 1e-8*h) ) && itsb < 100) )
       {
         if(itsb != 0)
-            alpha *= 0.5;
+            alpha *= 0.98;
         ipx_new.Point() = x + alpha*ddirection;
         MappedIntegrationPoint<2,2> mip_xnew(ipx_new,mip.GetTransformation());
         scafe_back.CalcShape(ipx_new,shape_back_new);
@@ -103,9 +135,9 @@ namespace ngfem
         Vec<2> value_new = -z + mip_xnew.GetPoint() + dvec_back_new;
         diffb = value_new - value_old - alpha*beta*jacobi_old*ddirection;
         itsb ++;
-        cout << "itsb = " << itsb << endl;
-        cout << "diffb(0) = " << diffb(0) << endl;
-        cout << "diffb(1) = " << diffb(1) << endl;
+        // cout << "itsb = " << itsb << endl;
+        // cout << "diffb(0) = " << diffb(0) << endl;
+        // cout << "diffb(1) = " << diffb(1) << endl;
       }
 
       cout << "alpha = " << alpha << endl;
@@ -115,23 +147,23 @@ namespace ngfem
       //alpha = 0.25;
       x += alpha*invd * diff;
 
-      cout << "x = " << x << endl;
-      cout << "z = " << z << endl;
+      // cout << "x = " << x << endl;
+      // cout << "z = " << z << endl;
       cout << "diff = " << diff << endl;
       cout << "its = " << its << endl;
-      cout << "dvec_back = " << dvec_back << endl;
-      cout << "dvec_forth = " << dvec_forth << endl;
-      cout << "dphi = " << dphi << endl;
-      cout << "dback_ref = " << dback_ref << endl;
-      cout << "dback = " << dback << endl;
+      // cout << "dvec_back = " << dvec_back << endl;
+      // cout << "dvec_forth = " << dvec_forth << endl;
+      // cout << "dphi = " << dphi << endl;
+      // cout << "dback_ref = " << dback_ref << endl;
+      // cout << "dback = " << dback << endl;
       //getchar();
 
-
+      cnt++;
+      cout << "cnt = " << cnt << endl;
       ipx.Point() = x;
 
     }
-
-
+    */
 
     scafe.CalcShape(ipx,shape);
     mat = 0.0;
--- spacetime/shiftedeval_debug.py
+++ spacetime/shiftedeval_debug.py
@@ -11,35 +11,29 @@ ngsglobals.msg_level = 1
 mesh = Mesh(unit_square.GenerateMesh(maxh=0.5))
 
 # H1-conforming finite element space
-fes = H1(mesh, order=5, dirichlet=[1,2,3,4])
+fes = H1(mesh, order=3, dirichlet=[1,2,3,4])
 fes_dfm = H1(mesh, order=3, dim=2)
 
-gfu = GridFunction(fes)
+gfu_new = GridFunction(fes)
 
-gfu1 = GridFunction(fes)
+gfu_old = GridFunction(fes)
 
 dfm_back = GridFunction(fes_dfm)
 #dfm_back.vec[37:] = 0.1
-dfm_back.Set(CoefficientFunction((0.05*sin(5*y),0.05*cos(5*x))))
+dfm_back.Set(CoefficientFunction((0.2*sin(5*y),0.2*cos(5*x))))
 for i in range(2*mesh.nv):
     dfm_back.vec[i] = 0.0
 dfm_forth = GridFunction(fes_dfm)
 
 mesh.SetDeformation(dfm_back)
-gfu.Set(sin(10*y))
+exact = sin(10*y)
+gfu_old.Set(exact)
+l2error_old = sqrt (Integrate ( (gfu_old-exact)*(gfu_old-exact), mesh, order=10))
 mesh.UnsetDeformation()
-gfu1.Set(shifted_eval(gfu,dfm_back,dfm_forth))
-Draw (gfu,mesh,"gfu")
-Draw (gfu1,mesh,"gfu1")
+Draw (gfu_old,mesh,"gfu_old")
 Draw (dfm_back,mesh,"dfmback")
+gfu_new.Set(shifted_eval(gfu_old,dfm_back,dfm_forth))
+Draw (gfu_new,mesh,"gfu_new")
 
-#gfu.vec.data = a.mat.Inverse(fes.FreeDofs(), inverse="sparsecholesky") * f.vec
-# print (u.vec)
-
-#
-## plot the solution (netgen-gui only)
-#Draw (gfu)
-#Draw (-grad(gfu), mesh, "Flux")
-#
-#exact = 16*x*(1-x)*y*(1-y)
-#print ("L2-error:", sqrt (Integrate ( (gfu-exact)*(gfu-exact), mesh)))
+print ("L2-error(old):", l2error_old)
+print ("L2-error(new):", sqrt (Integrate ( (gfu_new-exact)*(gfu_new-exact), mesh, order=10)))
