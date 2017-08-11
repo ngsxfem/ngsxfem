6be795fb3ca2de2b4675df455e8d1a2cda15f586
Christoph Lehrenfeld <lehrenfeld@math.uni-goettingen.de>
2017-08-11 08:05:55 +0200

    [numproc] removing numprocs

--- lsetcurving/lsetrefine.hpp
+++ lsetcurving/lsetrefine.hpp
@@ -16,21 +16,4 @@ namespace ngcomp
 
   void RefineAtLevelSet (shared_ptr<GridFunction> gf_lset_p1, double lower_lset_bound, double upper_lset_bound, LocalHeap & lh);
   
-  class NumProcLevelSetRefine : public NumProc
-  {
-  protected:
-    double lower_lset_bound;
-    double upper_lset_bound;
-    shared_ptr<GridFunction> gf_lset_p1;
-  public:
-    NumProcLevelSetRefine (shared_ptr<PDE> apde, const Flags & flags);
-    virtual ~NumProcLevelSetRefine() { }
-    virtual string GetClassName () const
-    {
-      return "NumProcLevelSetRefine";
-    }
-    
-    virtual void Do (LocalHeap & lh);
-  };
-  
 }
