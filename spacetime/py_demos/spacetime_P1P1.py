749f718e33bd16d4c406c2abbf32fcf7426f4d47
Christoph Lehrenfeld <lehrenfeld@math.uni-goettingen.de>
2018-06-08 10:21:32 +0200

    kind of second "example" (needs polishing)

--- spacetime/py_demos/spacetime_P1P1.py
+++ spacetime/py_demos/spacetime_P1P1.py
@@ -15,9 +15,55 @@ from xfem.lset_spacetime import *
 ngsglobals.msg_level = 1
 
 square = SplineGeometry()
-square.AddRectangle([-1,-0.75],[1,1.5])
-ngmesh = square.GenerateMesh(maxh=0.1, quad_dominated=False)
+square.AddRectangle([-1,-1],[1,1])
+ngmesh = square.GenerateMesh(maxh=0.08, quad_dominated=False)
 mesh = Mesh (ngmesh)
+
+#### expression for the time variable: 
+
+coef_told = Parameter(0)
+coef_delta_t = Parameter(0)
+tref = ReferenceTimeVariable()
+t = coef_told + coef_delta_t*tref
+
+#### the data: 
+# radius of disk (the geometry)
+r0 = 0.5
+
+if True:
+    ### case 1: analytical solution:
+    # position shift of the geometry in time
+    rho =  CoefficientFunction((1/(pi))*sin(2*pi*t))
+    #convection velocity:
+    d_rho = CoefficientFunction(2*cos(2*pi*t))
+    w = CoefficientFunction((0,d_rho)) 
+
+    # level set
+    r = sqrt(x**2+(y-rho)**2)
+    levelset= r - r0
+
+    # diffusion coefficient
+    alpha = 1
+
+    # solution and r.h.s.
+    Q = pi/r0   
+    u_exact = cos(Q*r) * sin(pi*t)
+    coeff_f = (Q/r * sin(Q*r) + (Q**2) * cos(Q*r)) * sin(pi*t) + pi * cos(Q*r) * cos(pi*t)
+    u_init = u_exact
+else:
+    ### case 2: no analytical solution
+    xy = CoefficientFunction((x,y))
+    center = r0 * CoefficientFunction((cos(2*pi*t),sin(2*pi*t)))
+    #convection velocity:
+    w = 2 * pi * CoefficientFunction((-y,x))
+    levelset = Norm(xy-center) - r0
+    u_init = exp(-(Norm(xy-1.5*center)))
+    u_exact = None
+    # diffusion coefficient
+    alpha = 0.05
+    coeff_f = CoefficientFunction(0)
+
+#### the discretization
 
 # polynomial order in time
 k_t = 1
@@ -35,29 +81,11 @@ tfe = ScalarTimeFE(k_t)
 st_fes = SpaceTimeFESpace(fes1,tfe, flags = {"dgjumps": True})
 
 #Fitted heat equation example
-tend = 0.5
+tend = 1
 delta_t = tend/32
+coef_delta_t.Set(delta_t)
 tnew = 0
 told = 0
-
-coef_told = Parameter(0)
-tref = ReferenceTimeVariable()
-t = coef_told + delta_t*tref
-
-
-# radius of disk (the geometry)
-r0 = 0.5
-
-# position shift of the geometry in time
-rho =  CoefficientFunction((1/(pi))*sin(2*pi*t))
-# velocity of position shift
-d_rho = CoefficientFunction(2*cos(2*pi*t))
-#convection velocity:
-w = CoefficientFunction((0,d_rho)) 
-
-# level set
-r = sqrt(x**2+(y-rho)**2)
-levelset= r - r0
 
 lset_p1 = GridFunction(st_fes)
 
@@ -66,15 +94,10 @@ SpaceTimeInterpolateToP1(levelset,coef_told,0.0,delta_t,lset_p1)
 lset_top = CreateTimeRestrictedGF(lset_p1,1.0)
 lset_bottom = CreateTimeRestrictedGF(lset_p1,0.0)
 
-# solution and r.h.s.
-Q = pi/r0   
-u_exact = cos(Q*r) * sin(pi*t)
-coeff_f = (Q/r * sin(Q*r) + (Q**2) * cos(Q*r)) * sin(pi*t) + pi * cos(Q*r) * cos(pi*t)
-
 gfu = GridFunction(st_fes)
 
 u_last = CreateTimeRestrictedGF(gfu,0)
-u_last.Set(CoefficientFunction(u_exact)) 
+u_last.Set(u_init) 
 
 u,v = st_fes.TnT()
 
@@ -84,7 +107,6 @@ Draw(lset_top,mesh,"lset")
 Draw(IfPos(-lset_top,CoefficientFunction((0,0)),CoefficientFunction((float('nan'),float('nan')))),mesh,"filter")
 visoptions.deformation = 1
 Draw(u_last, mesh,"u", sd=2, autoscale=False, min = -1, max = 1)
-
 
 lset_neg = { "levelset" : lset_p1, "domain_type" : NEG, "subdivlvl" : 0}
 lset_neg_bottom = { "levelset" : lset_bottom, "domain_type" : NEG, "subdivlvl" : 0}
@@ -101,7 +123,7 @@ hasneg_integrators_f = []
 patch_integrators_a = []
 hasneg_integrators_a.append(SpaceTimeNegBFI(form = -u*dt(v)))
 hasneg_integrators_a.append(SpaceTimeNegBFI(form = -delta_t*u*InnerProduct(w,grad(v))))
-hasneg_integrators_a.append(SpaceTimeNegBFI(form = delta_t*grad(u)*grad(v)))
+hasneg_integrators_a.append(SpaceTimeNegBFI(form = delta_t*alpha*grad(u)*grad(v)))
 hasneg_integrators_a.append(SymbolicBFI(levelset_domain = lset_neg_top, form = fix_t(u,1)*fix_t(v,1)))
 patch_integrators_a.append(SymbolicFacetPatchBFI(form = delta_t*1.05*h**(-2)*(u-u.Other())*(v-v.Other()),
                                                  skeleton=False, time_order=time_order))
@@ -156,12 +178,13 @@ while tend - told > delta_t/2:
     told = told + delta_t
     coef_told.Set(told)
     
-    # compute error at final time
-    l2error = sqrt(Integrate(lset_neg_top,(u_exact-u_last)*(u_exact-u_last),mesh))
-
-    # print time and error
-    print("\rt = {0:10}, l2error = {1:20}".format(told,l2error),end="")
-    
+    if u_exact != None:
+        # compute error at end of time slab
+        l2error = sqrt(Integrate(lset_neg_top,(u_exact-u_last)*(u_exact-u_last),mesh))
+        # print time and error
+        print("\rt = {0:10}, l2error = {1:20}".format(told,l2error),end="")
+    else:
+        print("\rt = {0:10}".format(told), end="")
     # Redraw:
     Redraw(blocking=True)
 
