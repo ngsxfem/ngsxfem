8ba213825943eac890d2a088afec9c6a5fadc17f
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2019-07-01 12:01:10 +0200

    Clean up CMakeLists.txt and setup.py

--- setup.py
+++ setup.py
@@ -13,14 +13,10 @@ except ImportError:
 
 from skbuild import setup
 
-#CMAKE_ARGS = ["-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9",
-#        "-DCMAKE_PREFIX_PATH=/Users/mhochsteger/Documents/Netgen.app/Contents/Resources",
-#        "-DCMAKE_OSX_SYSROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk",
-#        "-DBUILD_NGSOLVE=OFF",
-#        "-DSETUP_PY=ON",
-#        "-DUSE_CCACHE=ON"
-#        ]
+CMAKE_ARGS = [ "-DBUILD_NGSOLVE=OFF" ]
 
+# CMAKE_ARGS.append( "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9" )
+# CMAKE_ARGS.append( "-DCMAKE_OSX_SYSROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk" )
 
 setup(name="xfem",
       version="1.1.0",
