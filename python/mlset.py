37332cb91f5079a608c87682b9a7bf3d711c283c
Henry v. Wahl <henry.von-wahl@mathematik.uni-goettingen.de>
2020-03-20 11:04:47 +0100

    Unrolling Combined_DomainTypes into list: WIP -> operator.and_ for codim != 0 missing

--- python/mlset.py
+++ python/mlset.py
@@ -6,6 +6,7 @@ from xfem import *
 # import ngsolve
 import operator
 from copy import copy
+from itertools import permutations
 
 '''
 Below we draft objects for multi level set domains
@@ -13,6 +14,28 @@ Below we draft objects for multi level set domains
 
 
 class DomainTypeArray():
+
+    def __new__(cls, dtlist):
+        n = dtlist.count(ANY)
+        if n == 0:
+            return super(DomainTypeArray, cls).__new__(cls)
+        # Else: expand ANY top POS and NEG
+        dt_lists = [list(dtlist) for i in range(2**n)]
+        dt_per = list(set(permutations([NEG]*n + [POS]*n, n)))
+
+        for i, dt_list in enumerate(dt_lists):
+            j = 0
+            for k, dt in enumerate(dt_list):
+                if dt == ANY:
+                    dt_lists[i][k] = dt_per[i][j]
+                    j += 1
+
+        cdt_out = Combined_DomainType(operator.or_, DomainTypeArray(tuple(dt_lists[0])))
+        for dtl in dt_lists[1:]:
+            cdt_out |= DomainTypeArray(tuple(dtl))
+
+        return cdt_out
+
     def __init__(self, dtlist):
         self.dtlist = dtlist
         self.codim = 0
@@ -48,6 +71,9 @@ class DomainTypeArray():
 
     def __invert__(self):
         return Combined_DomainType(operator.inv, self)
+
+    def unroll(self):
+        return [self.dtlist]
 
 
 class Combined_DomainType():
@@ -95,8 +121,55 @@ class Combined_DomainType():
     def __invert__(self):
         return Combined_DomainType(operator.inv, self)
 
+    def __ior__(self, dta_b):
+        return Combined_DomainType(operator.or_, self, dta_b)
+
+    def __iand__(self, dta_b):
+        return Combined_DomainType(operator.and_, self, dta_b)
+
     def __str__(self):
         return "[" + self.dtlist1.__str__() + self.operator.__str__() + self.dtlist2.__str__() + "]{codim=" + str(self.codim) + "}"
+
+    def unroll(self):
+        if type(self.dtlist1) == DomainTypeArray and type(self.dtlist2) == DomainTypeArray:
+            # Combine two DomainTypeArrays
+            if self.operator == operator.and_:
+                for i, dt in enumerate(self.dtlist1):
+                    if dt != self.dtlist2.dtlist[i]:
+                        return []
+                else:
+                    return self.dtlist1.unroll()
+            elif self.operator == operator.or_:
+                return list(set(self.dtlist1.unroll() + elf.dtlist2.unroll()))
+            else:
+                raise Exception("Unexpected operator (1)...")
+        elif self.dtlist2 == None:
+            # Only one Combined_/Domain TypeArrays to unroll
+            if self.operator == operator.and_:
+                return []
+            elif self.operator == operator.or_:
+                return self.dtlist1.unroll()
+            elif self.operator == operator.inv:
+                n = len(self)
+                dt_per = set(permutations([NEG]*n + [POS]*n, n))
+                for dt in self.dtlist1.unroll():
+                    dt_per.remove(dt)
+                return list(dt_per)
+            else:
+                raise Exception("Unexpected operator (2)...")
+        elif (type(self.dtlist1) in [DomainTypeArray, Combined_DomainType] and 
+              type(self.dtlist2) in [DomainTypeArray, Combined_DomainType]):
+            # One Combined_TypeArray and one DomainTypeArray to unroll
+            dtl1_unrolled, dtl2_unrolled = self.dtlist1.unroll(), self.dtlist2.unroll()
+            if self.operator == operator.and_:
+                temp = set(dtl2_unrolled) 
+                return [dtt for dtt in dtl1_unrolled if dtt in temp] 
+            elif self.operator == operator.or_:
+                return list(set(dtl1_unrolled + dtl2_unrolled))
+            else:
+                raise Exception("Unexpected operator (3)...")
+        else:
+            raise Exception("Did not expect this situation")
 
 
 if __name__ == "__main__":
@@ -122,3 +195,56 @@ if __name__ == "__main__":
     print(cdtc)
     print(dta_a in cdtc)
     print(dta_b in cdtc)
+
+    input("Unroll tests")
+
+    dtt_1 = (POS, NEG)
+    dtt_1inv = [(NEG, POS), (POS, POS), (NEG, NEG)]
+    dta_1 = DomainTypeArray(dtt_1)
+    print("DomainTypeArray")
+    print("Unrolled: ", dta_1.unroll())
+    print("Expected: ", [dtt_1])
+    input("")
+
+    cdta_1 = Combined_DomainType(operator.or_, dta_1) 
+    print("Combined_DomainType")
+    print("Unrolled: ", cdta_1.unroll())
+    print("Expected: ", [dtt_1])
+    input("")
+
+    cdta_2 = Combined_DomainType(operator.and_, dta_1, dta_1) 
+    print("Combined_DomainType")
+    print("Unrolled: ", cdta_2.unroll())
+    print("Expected: ", [dtt_1])
+    input("")
+
+    cdta_3 = Combined_DomainType(operator.inv, dta_1) 
+    print("Combined_DomainType: inversion")
+    print("Unrolled: ", cdta_3.unroll())
+    print("Expected: ", dtt_1inv)
+    input("")
+
+    dtt_2 = (ANY, POS)
+    cdta_2 = DomainTypeArray(dtt_2)
+    print("Combined_DomainType with ANY")
+    print("Unrolled: ", cdta_2.unroll())
+    print("Expected: ", [(NEG, POS), (POS, POS)])
+    input("")
+
+    cdta_3 = ~dta_1 & cdta_2
+    print("Combined_DomainType inversion + and")
+    print("Unrolled: ", cdta_3.unroll())
+    print("Expected: ", [(NEG, POS), (POS, POS)])
+    input("")
+
+    cdta_4 = DomainTypeArray((ANY, ANY, ANY)) | DomainTypeArray((ANY, POS, NEG))
+    print("Combined_DomainType ANY + (ANY + or)")
+    print("Unrolled: ", cdta_4.unroll())
+    print("Expected: ", list(set(permutations([NEG]*3 + [POS]*3, 3))))
+    input("")
+
+    cdta_5 = cdta_3 | DomainTypeArray((NEG, NEG))
+    print("Combined_DomainType with or")
+    print("Unrolled: ", cdta_5.unroll())
+    print("Expected: ", [(NEG, POS), (POS, POS), (NEG, NEG)])
+    input("")
