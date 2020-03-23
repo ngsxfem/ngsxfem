45cd7b026e13a3342f50cfc9f3cebb05993d4825
Henry v. Wahl <henry.von-wahl@mathematik.uni-goettingen.de>
2020-03-23 16:14:51 +0100

    simply.py with convenience functions

--- py_tutorials/mlset/simple_mlcutinfo.py
+++ py_tutorials/mlset/simple_mlcutinfo.py
@@ -2,22 +2,17 @@
 example of a triangle described by three level set functions
 """
 
-# the constant pi
-from math import pi
-# ngsolve stuff
-from ngsolve import *
-# basic xfem functionality
-from xfem import *
-# basic geometry features (for the background mesh)
 from netgen.geom2d import SplineGeometry
+from ngsolve import *
+from xfem import *
+from xfem.mlset import *
 
-# We generate the background mesh of the domain and use a simplicial triangulation
-# To obtain a mesh with quadrilaterals use 'quad_dominated=True'
 
-square = SplineGeometry()
+# Background meshsquare = SplineGeometry()
 square.AddRectangle([-1,-0.5], [1,1.5], bc=1)
 mesh = Mesh (square.GenerateMesh(maxh=0.2, quad_dominated=False))
 
+# Level sets
 lsetp1_a = GridFunction(H1(mesh,order=1))
 lsetp1_b = GridFunction(H1(mesh,order=1))
 lsetp1_c = GridFunction(H1(mesh,order=1))
@@ -32,6 +27,7 @@ Draw (lsetp1_c, mesh, "lset_c")
 
 Draw (lsetp1_a * lsetp1_b * lsetp1_c, mesh, "lset_mult")
 
+# Show MultiLevelsetCutInfo functionality
 mlci = MultiLevelsetCutInfo(mesh,(lsetp1_a, lsetp1_b, lsetp1_c))
 
 hasneg = mlci.GetElementsOfType((NEG,NEG,NEG))
