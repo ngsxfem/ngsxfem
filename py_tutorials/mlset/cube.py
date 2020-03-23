707d82f62f8da08a13a66df3b011f01391b29cb5
Henry v. Wahl <henry.von-wahl@mathematik.uni-goettingen.de>
2020-03-23 18:30:58 +0100

    py_tutorials/cube{,2}.py adapted to new functionality.
    Note: abs within InterpolateToP1 does not work

--- py_tutorials/mlset/cube.py
+++ py_tutorials/mlset/cube.py
@@ -3,17 +3,19 @@
 	------------
 	Sharp cube defined by three levelsets.
 """
-import math
+
+# Import libraries
 from netgen.csg import *
 from ngsolve import *
 from xfem import *
+from xfem.mlset import *
 
-
+# Mesh
 geo = CSGeometry()
 geo.Add(OrthoBrick(Pnt(-0.8,-0.8,-0.8), Pnt(0.8,0.8,0.8)))
-mesh = Mesh(geo.GenerateMesh(maxh=0.4))
+mesh = Mesh(geo.GenerateMesh(maxh=0.75))
 
-
+# Level set
 V = H1(mesh,order=1)
 lsetp1_1, lsetp1_2, lsetp1_3 = GridFunction(V), GridFunction(V), GridFunction(V)
 
@@ -28,24 +30,22 @@ Draw(lsetp1_1,mesh,"lsetp1_1")
 Draw(lsetp1_2,mesh,"lsetp1_2")
 Draw(lsetp1_3,mesh,"lsetp1_3")
 
-volume = Integrate(levelset_domain={"levelset" : [lsetp1_1, lsetp1_2, lsetp1_3], "domain_type" : [NEG,NEG,NEG]},
-                   mesh=mesh,
-                   cf=1,
-                   order=0)
+# Integrate
+cube = DomainTypeArray(dtlist=[(NEG,NEG,NEG)])
+
+volume = Integrate(levelset_domain={"levelset": [lsetp1_1, lsetp1_2, lsetp1_3], 
+                                    "domain_type": cube.dtlist},
+                   mesh=mesh, cf=1, order=0)
 
 
 print("volume = {:10.8f}".format(volume))            
 print("volume error = {:4.3e}".format(abs(volume - 1)))  
+assert abs(volume - 1) < 1e-12
 
+surface_area = Integrate(levelset_domain={"levelset": [lsetp1_1, lsetp1_2, lsetp1_3], 
+                                          "domain_type": cube.Boundary().dtlist},
+                         mesh=mesh, cf=1, order=0)
 
-# inner = {"levelsets": (lsetp1_1, lsetp1_2, lsetp1_3),
-# 		 "domain_type" : (NEG,NEG,NEG)
-# 		}
-
-# boundary =  {"levelsets": (lsetp1_1, lsetp1_2, lsetp1_3),
-# 		 	 "domain_type" : (IF,NEG,NEG)|(NEG,IF,NEG)|(NEG,NEG,IF)
-# 			}
-
-# outer = {"levelsets": (lsetp1_1, lsetp1_2, lsetp1_3),
-# 		 "domain_type" : ~(NEG,NEG,NEG)
-# 		}
+print("surface_area = {:10.8f}".format(surface_area))            
+print("surface_area error = {:4.3e}".format(abs(surface_area - 6)))  
+assert abs(surface_area - 6) < 1e-12
--- py_tutorials/mlset/cube2.py
+++ py_tutorials/mlset/cube2.py
@@ -1,20 +1,25 @@
 """
 	Description:
 	------------
-	Sharp cube defined by three levelsets.
+	Sharp cube defined by six level sets, one for each face.
 """
+
+# Import libraries
 from netgen.csg import *
 from ngsolve import *
 from xfem import *
+from xfem.mlset import *
 
-
+# Mesh
 geo = CSGeometry()
 geo.Add(OrthoBrick(Pnt(-0.8,-0.8,-0.8), Pnt(0.8,0.8,0.8)))
 mesh = Mesh(geo.GenerateMesh(maxh=1))
 
-
+# Level sets
 V = H1(mesh,order=1)
-lsetp1_x_upper, lsetp1_x_lower, lsetp1_y_upper, lsetp1_y_lower, lsetp1_z_upper, lsetp1_z_lower = GridFunction(V), GridFunction(V), GridFunction(V), GridFunction(V), GridFunction(V), GridFunction(V)
+lsetp1_x_upper, lsetp1_x_lower = GridFunction(V), GridFunction(V)
+lsetp1_y_upper, lsetp1_y_lower = GridFunction(V), GridFunction(V)
+lsetp1_z_upper, lsetp1_z_lower = GridFunction(V), GridFunction(V)
 
 InterpolateToP1( x - 0.5, lsetp1_x_upper)
 InterpolateToP1( x + 0.5, lsetp1_x_lower)
@@ -23,18 +28,22 @@ InterpolateToP1( y + 0.5, lsetp1_y_lower)
 InterpolateToP1( z - 0.5, lsetp1_z_upper)
 InterpolateToP1( z + 0.5, lsetp1_z_lower)
 
-input("Codim 0")
-volume = Integrate(levelset_domain = {"levelset" : [lsetp1_x_upper, lsetp1_x_lower, 
-                                                    lsetp1_y_upper, lsetp1_y_lower, 
-                                                    lsetp1_z_upper, lsetp1_z_lower],
-                                      "domain_type" : [NEG, POS, NEG, POS, NEG, POS]},
+# Integrate
+cube = DomainTypeArray(dtlist=[(NEG, POS, NEG, POS, NEG, POS)])
+
+# Compute volume
+volume = Integrate(levelset_domain={"levelset": [lsetp1_x_upper, lsetp1_x_lower, 
+                                                 lsetp1_y_upper, lsetp1_y_lower, 
+                                                 lsetp1_z_upper, lsetp1_z_lower],
+                                    "domain_type": cube.dtlist},
                               mesh=mesh, cf=1, order=0)
 
 print("volume = {:10.8f}".format(volume))            
 print("volume error = {:4.3e}".format(abs(volume - 1)))
+assert abs(volume - 1)
 
-input("Now codim 1")
 
+# Compute are of surfaces
 for i in range(6):
     domian = [NEG, POS, NEG, POS, NEG, POS]
     domian[i] = IF
@@ -42,48 +51,45 @@ for i in range(6):
     area = Integrate(levelset_domain = {"levelset" : [lsetp1_x_upper, lsetp1_x_lower, 
                                                       lsetp1_y_upper, lsetp1_y_lower, 
                                                       lsetp1_z_upper, lsetp1_z_lower],
-                                        "domain_type" : domian},
+                                        "domain_type" : tuple(domian)},
                      mesh=mesh, cf=1, order=0)
 
-    print("area{:d} = {:10.8f}".format(i, area))                     
-    print("area{:d} error = {:4.2e}".format(i, abs(area-1)))
+    print("area_face{:d} = {:10.8f}".format(i, area))                     
+    print("area_face{:d} error = {:4.2e}".format(i, abs(area - 1)))
+    assert abs(area-1) < 1e-12
 
-    input("")   
+surface_area = Integrate(levelset_domain={"levelset": [lsetp1_x_upper, lsetp1_x_lower, 
+                                                 lsetp1_y_upper, lsetp1_y_lower, 
+                                                 lsetp1_z_upper, lsetp1_z_lower],
+                                    "domain_type": cube.Boundary().dtlist},
+                              mesh=mesh, cf=1, order=0)
 
-input("Now codim 3")
+print("surface_area = {:10.8f}".format(surface_area))            
+print("surface_area error = {:4.3e}".format(abs(surface_area - 6)))
+assert abs(surface_area - 6)
 
+# Point evaluation
+point_domains = {"ppp": (IF, POS, IF, POS, IF, POS),
+                 "ppn": (IF, POS, IF, POS, NEG, IF),
+                 "pnp": (IF, POS, NEG, IF, IF, POS), 
+                 "npp": (NEG, IF, IF, POS, IF, POS),
+                 "pnn": (IF, POS, NEG, IF, NEG, IF),
+                 "npn": (NEG, IF, IF, POS, NEG, IF),
+                 "nnp": (NEG, IF, NEG, IF, IF, POS),
+                 "nnn": (NEG, IF, NEG, IF, NEG, IF) }
 
-point_domains = {"ppp": [IF, POS, IF, POS, IF, POS],
-                 "ppn": [IF, POS, IF, POS, NEG, IF],
-                 "pnp": [IF, POS, NEG, IF, IF, POS], 
-                 "npp": [NEG, IF, IF, POS, IF, POS],
-                 "pnn": [IF, POS, NEG, IF, NEG, IF],
-                 "npn": [NEG, IF, IF, POS, NEG, IF],
-                 "nnp": [NEG, IF, NEG, IF, IF, POS],
-                 "nnn": [NEG, IF, NEG, IF, NEG, IF] }
-
-vals = {"ppp": 1.5,
-        "ppn": 0.5,
-        "pnp": 0.5,
-        "npp": 0.5,
-        "pnn": -0.5,
-        "npn": -0.5,
-        "nnp": -0.5,
-        "nnn": -1.5
-}
+vals = {"ppp": 1.5, "ppn": 0.5, "pnp": 0.5, "npp": 0.5, "pnn": -0.5, 
+        "npn": -0.5, "nnp": -0.5, "nnn": -1.5 }
     
-
-
 for i, (key, domain) in enumerate(point_domains.items()):
 
-    point = Integrate(levelset_domain = {"levelset" : [lsetp1_x_upper, lsetp1_x_lower, 
+    point = Integrate(levelset_domain = {"levelset": [lsetp1_x_upper, lsetp1_x_lower, 
                                                       lsetp1_y_upper, lsetp1_y_lower, 
                                                       lsetp1_z_upper, lsetp1_z_lower],
-                                        "domain_type" : domain},
+                                        "domain_type": domain},
                      mesh=mesh, cf=x+y+z, order=0)
 
     
     print("point{:d} = {:10.8f}".format(i, point))                     
     print("point{:d} error = {:4.2e}".format(i, abs(point-vals[key])))
-    assert abs(point-vals[key]) < 1e-12
-    input("")
+    assert abs(point - vals[key]) < 1e-12
