32d5f85b1fc3f0a2cf60037c7aeedf4f3f7d9430
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2020-05-09 22:35:18 +0200

    cmake setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,74 @@
+#!/usr/bin/env python
+import os
+import re
+import sys
+import platform
+import subprocess
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+        for ext in self.extensions:
+            self.build_extension(ext)
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        # required for auto-detection of auxiliary "native" libs
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable,
+                      '-DCMAKE_CXX_COMPILER=ngscxx',
+                      '-DCMAKE_LINKER=ngsld',
+                      '-DBUILD_NGSOLVE=OFF']
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+        if platform.system() == "Windows":
+            #not expected to work... (but who knows..)
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
-try:
-    import skbuild
-except ImportError:
-    print("")
-    print("***********************************************************************************")
-    print("")
-    print("skbuild not found, please execute:")
-    print("pip3 install scikit-build")
-    print("")
-    print("***********************************************************************************")
-    print("")
-
-from skbuild import setup
-
-CMAKE_ARGS = [ "-DBUILD_NGSOLVE=OFF" ]
-
-# CMAKE_ARGS.append( "-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9" )
-# CMAKE_ARGS.append( "-DCMAKE_OSX_SYSROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk" )
-
-setup(name="xfem",
-      version="1.1.0",
-      description="",
-      packages=["xfem", "xfem.lsetcurv", "xfem.lset_spacetime", "xfem.utils"],
-      package_dir={"xfem" : "python",
-                   "xfem.lsetcurv" : "lsetcurving",
-                   "xfem.lset_spacetime" : "spacetime",
-                   "xfem.utils" : "utils"},
-      cmake_args=CMAKE_ARGS)
-
+setup(
+    name='xfem',
+    version='1.3-dev',
+    author='Christoph Lehrenfeld',
+    author_email='lehrenfeld@math.uni-goettingen.de',
+    description='(ngs)xfem is an Add-on library to Netgen/NGSolve for unfitted/cut FEM.',
+    long_description='(ngs)xfem is an Add-on library to Netgen/NGSolve which enables the use of unfitted finite element technologies known as XFEM, CutFEM, TraceFEM, Finite Cell, … . ngsxfem is an academic software. Its primary intention is to facilitate the development and validation of new numerical methods.',
+    ext_modules=[CMakeExtension('ngsxfem_py')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    packages=["xfem", "xfem.cutmg", "xfem.lsetcurv", "xfem.lset_spacetime", "xfem.mlset", "xfem.utils"],
+    package_dir={"xfem" : "python",
+                 "xfem.cutmg" : "python",
+                 "xfem.lsetcurv" : "lsetcurving",
+                 "xfem.lset_spacetime" : "spacetime",
+                 "xfem.mlset" : "python",
+                 "xfem.utils" : "utils"},
+    
+    zip_safe=False,
+)
