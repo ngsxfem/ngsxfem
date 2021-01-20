0d5dc6d19d8e8013c5018766f928d76a0fd6a255
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2021-01-20 09:56:57 +0100

    Ci... debug

--- .ci/ci_ctests.sh
+++ .ci/ci_ctests.sh
@@ -6,6 +6,10 @@ ls -al .
 echo "cmake-tests"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PWD}/install/ngsxfem/lib/python3/dist-packages/xfem/"
 export PYTHONPATH="${PYTHONPATH}:${PWD}/install/ngsxfem/lib/python3/dist-packages"
+echo "${PWD}/install/ngsxfem/lib/python3/dist-packages"
+ls -al ${PWD}/install/ngsxfem/lib/python3/dist-packages
+export | grep PYTHONPATH
+
 cd build
 
 if [ $1 == "tutorial" ]; then
