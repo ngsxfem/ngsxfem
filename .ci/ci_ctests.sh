b2c618623268e62eafe09c12a80883224db57de9
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2021-01-20 11:25:05 +0100

    Ci... debug

--- .ci/ci_ctests.sh
+++ .ci/ci_ctests.sh
@@ -4,8 +4,8 @@ set -e
 echo "pwd: ${PWD}"
 ls -al .
 echo "cmake-tests"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PWD}/install/ngsxfem/lib/python3/dist-packages/xfem/"
-export PYTHONPATH="${PYTHONPATH}:${PWD}/install/ngsxfem/lib/python3/dist-packages"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PWD}/install/lib/python3/dist-packages/xfem/"
+export PYTHONPATH="${PYTHONPATH}:${PWD}/install/lib/python3/dist-packages"
 echo "${PWD}/install/ngsxfem/lib/python3/dist-packages"
 ls -al ${PWD}/install/ngsxfem/lib/python3/dist-packages
 export | grep PYTHONPATH
