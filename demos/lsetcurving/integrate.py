1307394c4e5201a2ff4522b343eabd189751c22f
Henry Maximilian von Wahl <henry.von-wahl@mathematik.uni-goettingen.de>
2021-01-30 16:01:11 +0100

    rewritten `demos/lsetcuring/integrate.py` for PEP8 conformity and added introductory text

--- demos/lsetcurving/integrate.py
+++ demos/lsetcurving/integrate.py
@@ -1,75 +1,106 @@
-# integration on lset domains
-from math import pi
-# ngsolve stuff
+"""
+In this example we integrate over a circle with radius 1 to illustrate
+the higher order convergence achieved through the isoparametric mapping
+approach. The mesh is deformed in order to map the P1 level set
+approximation onto a higher-order approximation of the smooth level set,
+thereby overcoming the geometry error of h^2 made by the piecewise
+linear level set approximation and recovering the full integration
+order.
+
+Literature
+----------
+[1] C. Lehrenfeld. High order unfitted finite element methods on level
+    set domains using isoparametric mappings. Computer Methods in
+    Applied Mechanics and Engineering, 300:716-733, 2016.
+[2] C. Lehrenfeld. A higher order isoparametric fictitious domain method
+    for level set domains. In S. P. A. Bordas, E. Burman, M. G. Larson,
+    and M. A. Olshanskii, editors, Geometrically Unfitted Finite Element
+    Methods and Applications, pages 65-92. Springer International
+    Publishing, 2017.
+"""
+
+# ------------------------------ LOAD LIBRARIES -------------------------------
+from netgen.geom2d import SplineGeometry
 from ngsolve import *
-# basic xfem functionality
+
 from xfem import *
-# For LevelSetAdaptationMachinery
 from xfem.lsetcurv import *
 
-#square domain [-1,1]x[-1,1]
-def Make2DProblem(maxh=2):
-    from netgen.geom2d import SplineGeometry
-    square = SplineGeometry()
-    square.AddRectangle([-1,-1],[1,1],bc=1)
-    mesh = Mesh (square.GenerateMesh(maxh=maxh, quad_dominated=False))
-    return mesh;
+from math import pi
 
-# circle with radius 0.5
-levelset = sqrt(x*x+y*y)-0.5
-referencevals = { POS : 4.0-0.25*pi, NEG : 0.25*pi, IF : pi }
 
-mesh = Make2DProblem(maxh=0.5)
-
+# -------------------------------- PARAMETERS ---------------------------------
+maxh = 0.5
 order = 5
-lsetmeshadap = LevelSetMeshAdaptation(mesh, order=order, threshold=0.2, discontinuous_qn=True)
 
-errors_uncurved = dict()
-errors_curved = dict()
-eoc_uncurved = dict()
-eoc_curved = dict()
+maxreflvl = 6
 
-for key in [NEG,POS,IF]:
-    errors_curved[key] = []
-    errors_uncurved[key] = []
+
+# ----------------------------------- MAIN ------------------------------------
+def Make2DProblem(maxh=2):
+    square = SplineGeometry()
+    square.AddRectangle([-1, -1], [1, 1], bc=1)
+    mesh = Mesh(square.GenerateMesh(maxh=maxh, quad_dominated=False))
+    return mesh
+
+
+# Circle with radius 0.5
+levelset = sqrt(x * x + y * y) - 0.5
+integrand = CoefficientFunction(1.0)
+referencevals = {POS: 4.0 - 0.25 * pi, NEG: 0.25 * pi, IF: pi}
+
+mesh = Make2DProblem(maxh=maxh)
+
+# Level set mesh adaptation machinery
+lsetmeshadap = LevelSetMeshAdaptation(mesh, order=order, threshold=0.2,
+                                      discontinuous_qn=True)
+
+# Containers to compute error convergence
+err_uncurved, err_curved = {}, {}
+eoc_uncurved, eoc_curved = {}, {}
+
+for key in [NEG, POS, IF]:
+    err_curved[key] = []
+    err_uncurved[key] = []
     eoc_curved[key] = []
     eoc_uncurved[key] = []
 
-
-for reflevel in range(6):
-
+# Main loop
+for reflevel in range(maxreflvl):
     if(reflevel > 0):
         mesh.Refine()
-    
-    f = CoefficientFunction (1.0)
 
-    for key in [NEG,POS,IF]:
-        integrals_uncurved = Integrate(levelset_domain = { "levelset" : levelset, "domain_type" : key},
-                                       cf=f, mesh=mesh, order = order)
-        # Applying the mesh deformation
+    for key in [NEG, POS, IF]:
+        lset_dom = {"levelset": levelset, "domain_type": key}
+
+        integrals_uncurved = Integrate(levelset_domain=lset_dom, cf=integrand,
+                                       mesh=mesh, order=order)
+
+        # Apply the mesh deformation
         deformation = lsetmeshadap.CalcDeformation(levelset)
         mesh.SetDeformation(deformation)
-        integrals_curved = Integrate(levelset_domain = { "levelset" : levelset, "domain_type" : key},
-                                       cf=f, mesh=mesh, order = order)
-        # Unapply the mesh deformation (for refinement)
+        integrals_curved = Integrate(levelset_domain=lset_dom, cf=integrand,
+                                     mesh=mesh, order=order)
+        # Undo the mesh deformation (for refinement)
         mesh.UnsetDeformation()
 
-        errors_curved[key].append(abs(integrals_curved - referencevals[key]))
-        errors_uncurved[key].append(abs(integrals_uncurved - referencevals[key]))
-    # refine cut elements:
+        err_curved[key].append(abs(integrals_curved - referencevals[key]))
+        err_uncurved[key].append(abs(integrals_uncurved - referencevals[key]))
+
+    # Mark cut elements for refinement:
     RefineAtLevelSet(gf=lsetmeshadap.lset_p1)
 
-for key in [NEG,POS,IF]:
-    eoc_curved[key] = [log(a/b)/log(2) for (a,b) in zip (errors_curved[key][0:-1],errors_curved[key][1:]) ]
-    eoc_uncurved[key] = [log(a/b)/log(2) for (a,b) in zip (errors_uncurved[key][0:-1],errors_uncurved[key][1:]) ]
+for key in [NEG, POS, IF]:
+    eoc_curved[key] = [log(a / b) / log(2) for (a, b)
+                       in zip(err_curved[key][:-1], err_curved[key][1:])]
+    eoc_uncurved[key] = [log(a / b) / log(2) for (a, b)
+                         in zip(err_uncurved[key][:-1], err_uncurved[key][1:])]
 
-print("errors (uncurved):  \n{}\n".format(errors_uncurved))
-print("   eoc (uncurved):  \n{}\n".format(   eoc_uncurved))
-print("errors (  curved):  \n{}\n".format(  errors_curved))
-print("   eoc (  curved):  \n{}\n".format(     eoc_curved))
+print("errors (uncurved):  \n{}\n".format(err_uncurved))
+print("   eoc (uncurved):  \n{}\n".format(eoc_uncurved))
+print("errors (  curved):  \n{}\n".format(err_curved))
+print("   eoc (  curved):  \n{}\n".format(eoc_curved))
 
-Draw(levelset,mesh,"levelset")
-Draw(lsetmeshadap.deform,mesh,"deformation")
-Draw(lsetmeshadap.lset_p1,mesh,"levelset(P1)")
-
-
+Draw(levelset, mesh, "levelset")
+Draw(lsetmeshadap.deform, mesh, "deformation")
+Draw(lsetmeshadap.lset_p1, mesh, "levelset(P1)")
