cb3fb2f42690ca6ea0c43a695a91ad2c2f42cf1f
Henry Maximilian von Wahl <henry.von-wahl@mathematik.uni-goettingen.de>
2021-01-30 16:45:24 +0100

    rewritten `demos/lsetcuring/lsetgeoms.py` for PEP8 conformity and added introductory text

--- demos/lsetcurving/lsetgeoms.py
+++ demos/lsetcurving/lsetgeoms.py
@@ -1,64 +1,80 @@
-# integration on lset domains
-from math import pi
-# ngsolve stuff
+"""
+This examples shows a number of pre-implemented 3d level set geometries,
+computes the distance between the smooth level set and the P1
+approximation as well as the order of convergence thereof.
+"""
+
+# ------------------------------ LOAD LIBRARIES -------------------------------
+from netgen.csg import CSGeometry
 from ngsolve import *
-# basic xfem functionality
+from ngsolve.internal import *
+
 from xfem import *
-# For LevelSetAdaptationMachinery
 from xfem.lsetcurv import *
+from xfem.utils import LevelsetExamples, BoundingBoxes
 
 import sys
 
-# For levelsets
-from xfem.utils import LevelsetExamples, BoundingBoxes
 
-from netgen.csg import CSGeometry, OrthoBrick, Pnt
-from netgen.meshing import MeshingParameters
+# -------------------------------- PARAMETERS ---------------------------------
+maxh = 1.0
+order = 2
+maxreflvl = 5
 
-# from ngsolve.internal import *
-# viewoptions.clipping.enable=1
+
+# ----------------------------------- MAIN ------------------------------------
+# Visual options for the Netgen GUI
+# viewoptions.clipping.enable = 1
 # viewoptions.clipping.nx = 0.0
 # viewoptions.clipping.ny = 1
 # viewoptions.clipping.nz = 0.0
-# visoptions.mminval=0.0
-# visoptions.mmaxval=0.0
+# visoptions.mminval = 0.0
+# visoptions.mmaxval = 0.0
 # visoptions.autoscale = False
 # visoptions.isosurf = 1
 # visoptions.numiso = 1
 # visoptions.subdivisions = 1
 
-for lsetgeom in ["cheese","torus","dziukelliott","dziuk88","sphere"]:
+# Main loop
+for lsetgeom in ["cheese", "torus", "dziukelliott", "dziuk88", "sphere"]:
+    print('Gemometry: ', lsetgeom)
+
     geom = CSGeometry()
-    geom.Add (BoundingBoxes[lsetgeom])
-    mesh = Mesh(geom.GenerateMesh (maxh=1.0))
+    geom.Add(BoundingBoxes[lsetgeom])
+    mesh = Mesh(geom.GenerateMesh(maxh=maxh))
     levelset = LevelsetExamples[lsetgeom]
 
-    order = 2
-    lsetmeshadap = LevelSetMeshAdaptation(mesh, order=order, threshold=100, discontinuous_qn=True)
+    lsetmeshadap = LevelSetMeshAdaptation(mesh, order=order, threshold=100,
+                                          discontinuous_qn=True)
 
-    Draw(levelset,mesh,"levelset")
-    Draw(lsetmeshadap.deform,mesh,"deformation")
-    Draw(lsetmeshadap.lset_p1,mesh,"levelset(P1)")
+    Draw(levelset, mesh, "levelset")
+    Draw(lsetmeshadap.deform, mesh, "deformation")
+    Draw(lsetmeshadap.lset_p1, mesh, "levelset(P1)")
 
     distances = []
-    for reflevel in range(5):
+    for reflevel in range(maxreflvl):
 
         if(reflevel > 0):
             mesh.Refine()
 
-        # Applying the mesh deformation
+        # Apply the mesh deformation
         deformation = lsetmeshadap.CalcDeformation(levelset)
-        
         mesh.SetDeformation(deformation)
-        distances.append(lsetmeshadap.CalcMaxDistance(levelset));
+        distances.append(lsetmeshadap.CalcMaxDistance(levelset))
+
+        # Undo mesh deformation (for refinement)
         mesh.UnsetDeformation()
 
-        # refine cut elements:
+        # Refine cut elements:
         RefineAtLevelSet(gf=lsetmeshadap.lset_p1)
 
+        # Post-processing
         Redraw(blocking=True)
-        eoc = [ log(distances[i-1]/distances[i])/log(2) for i in range(1,len(distances))]
+        eoc = [log(distances[i - 1] / distances[i]) / log(2)
+               for i in range(1, len(distances))]
         print("distances = {}".format(distances))
         print("eoc = {}".format(eoc))
-    if hasattr(sys,'ps1') or sys.flags.interactive or 'running_in_netgen' in dir():
+
+    if (hasattr(sys, 'ps1') or sys.flags.interactive
+            or 'running_in_netgen' in dir()):
         input("press ENTER to go to next geometry (or finish)")
