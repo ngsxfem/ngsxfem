1e9a1db6847845dc4f79127dad8facf43e6d80dd
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2021-02-06 15:57:40 +0100

    add IntegrationPointExtrema

--- cutint/python_cutint.cpp
+++ cutint/python_cutint.cpp
@@ -50,7 +50,6 @@ void ExportNgsx_cutint(py::module &m)
           MyMutex mutex_ip_cont;
           MyLock lock_ip_cont(mutex_ip_cont);
 
-          Array<int> dnums;
           ma->IterateElements
             (VOL, lh, [&] (Ngs_Element el, LocalHeap & lh)
              {
@@ -155,6 +154,110 @@ heapsize : int
   heapsize for local computations.
 )raw_string"));
 
+    m.def("IntegrationPointExtrema",
+        [](py::dict lsetdom,
+           shared_ptr<MeshAccess> ma,
+           PyCF cf,
+           int heapsize)
+        {
+          static Timer t ("IntegrationPointExtrema"); RegionTimer reg(t);
+
+          shared_ptr<LevelsetIntegrationDomain> lsetintdom = PyDict2LevelsetIntegrationDomain(lsetdom);
+          bool space_time = lsetintdom->GetTimeIntegrationOrder() >= 0;
+          LocalHeap lh(heapsize, "lh-IntegrationPointExtrema");
+
+          double min = 1e99;
+          double max = -1e99;
+
+          int DIM = ma->GetDimension();
+          int cfdim = cf->Dimension();
+
+          ma->IterateElements
+            (VOL, lh, [&] (Ngs_Element el, LocalHeap & lh)
+             {
+               auto & trafo = ma->GetTrafo (el, lh);
+
+               const IntegrationRule * ir;
+               Array<double> wei_arr;
+               tie (ir, wei_arr) = CreateCutIntegrationRule(*lsetintdom,trafo,lh);
+
+               if (ir != nullptr)
+               {
+                 BaseMappedIntegrationRule & mir = trafo(*ir, lh);
+                 FlatMatrix<> val(mir.Size(), 1, lh);
+
+                 cf -> Evaluate (mir, val);
+
+                 double lmin = 1e99;
+                 double lmax = -1e99;
+                 for (int i = 0; i < mir.Size(); i++)
+                 {
+                     if (val(i,0) > lmax) 
+                       lmax = val(i,0);
+                     if (val(i,0) < lmin) 
+                       lmin = val(i,0);
+                 }
+
+                 AtomicMax(max,lmax);
+                 AtomicMin(min,lmin);
+               }
+             });
+          py::list minmax;
+          minmax.append(min);
+          minmax.append(max);
+          return py::tuple(minmax);
+        },
+        py::arg("levelset_domain"),
+        py::arg("mesh"),
+        py::arg("cf")=PyCF(make_shared<ConstantCoefficientFunction>(0.0)),
+        py::arg("heapsize")=1000000,
+        docu_string(R"raw_string(
+Determine minimum and maximum on integration points on a level set domain. The sampling uses the same
+integration rule as in Integrate and is determined by 'order' w.r.t. a (multi-)linear
+approximation of the level set function. At first, this implies that the accuracy will, in general,
+only be second order. However, if the isoparametric approach is used (cf. lsetcurving functionality)
+this will be improved.
+
+Parameters
+
+levelset_domain : dictionary which provides levelsets, domain_types and integration specifica:
+  important keys are "levelset", "domain_type", "order", the remainder are additional:
+
+    "levelset" : ngsolve.CoefficientFunction or a list thereof
+      CoefficientFunction that describes the geometry. In the best case lset is a GridFunction of an
+      FESpace with scalar continuous piecewise (multi-) linear basis functions.
+
+
+    "order" : int
+      integration order.
+
+    "domain_type" : {NEG,POS,IF} (ENUM) or a list (of lists) thereof
+      Integration on the domain where either:
+      * the level set function is negative (NEG)
+      * the level set function is positive (POS)
+      * the level set function is zero     (IF )
+
+    "subdivlvl" : int
+      On simplex meshes a subtriangulation is created on which the level set function lset is
+      interpolated piecewise linearly. Based on this approximation, the integration rule is
+      constructed. Note: this argument only works on simplices without space-time and without 
+      multiple levelsets.
+
+    "time_order" : int
+      integration order in time for space-time integration
+
+    "quad_dir_policy" : int
+      policy for the selection of the order of integration directions
+
+mesh : 
+  Mesh to integrate on (on some part) 
+
+cf : ngsolve.CoefficientFunction
+  the integrand
+
+heapsize : int
+  heapsize for local computations.
+)raw_string"));
 
 
 
