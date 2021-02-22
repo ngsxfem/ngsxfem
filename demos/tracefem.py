63a9b437c16911bdee83e03b84418f1bff6786c0
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2021-02-22 14:08:24 +0100

    tracefem merge

--- demos/tracefem.py
+++ demos/tracefem.py
@@ -1,47 +1,85 @@
 """
+In this example we solve a scalar Laplace-Beltrami problem with a
+similar discretisation method to the one used in tracefem.py. However,
+we use a 3D (background mesh dimension) problem and higher order method
+this time. To be robust w.r.t. the interface position also in the 
+condition number we use the normal diffusion stabilization, cf.[1,2].
+
+Used features:
+-------------------------
+* Higher order eometry approximation, cf. ngsxfem-jupyter tutorial
+  intlset.ipynb from https://github.com/ngsxfem/ngsxfem-jupyter/
+* Compressed finite element space to condense the system to active dofs
+* Visualization: The visualization of the solution is most convenient
+  with paraview and the generated vtk file.
+
+Literature:
+-----------
+[1] J. Grande, C. Lehrenfeld, A. Reusken, Analysis of a high-order trace 
+    finite element method for PDEs on level set surfaces. 
+    SIAM Journal on Numerical Analysis, 56(1):228-255, 2018.
+[2] E. Burman, P. Hansbo, M. G. Larson, A. Massing, Cut  finite  element  
+    methods  for  partial  differential  equations  on  embedded  manifolds  
+    of  arbitrary  codimensions.  ESAIM: M2AN, 52(6):2247-2282, 2018.
 """
 
 # ------------------------------ LOAD LIBRARIES -------------------------------
-from netgen.geom2d import SplineGeometry
+from netgen.csg import CSGeometry, OrthoBrick, Pnt
 from ngsolve import *
 from ngsolve.internal import *
 from xfem import *
-
+from xfem.lsetcurv import *
+from math import pi
 
 # -------------------------------- PARAMETERS ---------------------------------
-# MEsh diameter
-maxh = 0.3
+# Mesh diameter
+maxh = 0.6
+# Refine cut elements of (initial) mesh
+n_cut_ref = 2
 # Polynomial order of FE space
-order = 1
-# Extra subdivisions of cut elements to generate quadrature rule
-subdivlvl = 0
+order = 2
 
+# Problem parameters
+reac_cf = 1
+diff_cf = 1
+
+# Geometry
+cube = CSGeometry()
+cube.Add(OrthoBrick(Pnt(-1.41, -1.41, -1.41), Pnt(1.41, 1.41, 1.41)))
+mesh = Mesh(cube.GenerateMesh(maxh=maxh, quad_dominated=False))
+
+levelset = sqrt(x**2 + y**2 + z**2) - 1
+exact = sin(pi * z)
+coef_f = (sin(pi * z) * (diff_cf * pi * pi * (1 - z * z) + reac_cf)
+          + diff_cf * cos(pi * z) * 2 * pi * z)
 
 # ----------------------------------- MAIN ------------------------------------
-# geometry
-square = SplineGeometry()
-square.AddRectangle([-1.5, -1.5], [1.5, 1.5], bc=1)
-mesh = Mesh(square.GenerateMesh(maxh=maxh, quad_dominated=False))
+# preliminary refinement
+for i in range(n_cut_ref):
+    lsetp1 = GridFunction(H1(mesh, order=1))
+    InterpolateToP1(levelset, lsetp1)
+    RefineAtLevelSet(lsetp1)
+    mesh.Refine()
 
-levelset = sqrt(x * x + y * y) - 0.7
 
-lset_approx = GridFunction(H1(mesh, order=1))
-InterpolateToP1(levelset, lset_approx)
+# Class to compute the mesh transformation needed for higher order accuracy
+#  * order: order of the mesh deformation function
+#  * threshold: barrier for maximum deformation (to ensure shape regularity)
+lsetmeshadap = LevelSetMeshAdaptation(mesh, order=order, threshold=1000,
+                                      discontinuous_qn=True)
+deformation = lsetmeshadap.CalcDeformation(levelset)
+lset_approx = lsetmeshadap.lset_p1
 
-# Extended FESpace
-VhG = H1(mesh, order=order, dirichlet=[])
+# Background FESpace
+Vh = H1(mesh, order=order, dirichlet=[])
 
-# Overwrite freedofs of VhG to mark only dofs that are involved in the
-# cut problem
 ci = CutInfo(mesh, lset_approx)
 ba_IF = ci.GetElementsOfType(IF)
-cf_IF = BitArrayCF(ba_IF)
-freedofs = VhG.FreeDofs()
-freedofs &= GetDofsOfElements(VhG, ba_IF)
+VhG = Compress(Vh, GetDofsOfElements(Vh, ba_IF))
 
 gfu = GridFunction(VhG)
 
-# Coefficients / parameters:
+# coefficients / parameters:
 n = 1.0 / Norm(grad(lset_approx)) * grad(lset_approx)
 h = specialcf.mesh_size
 
@@ -51,32 +89,32 @@ def P(u):
     return u - (u * n) * n
 
 
-# Expressions of test and trial functions:
-u, v = VhG.TnT()
+u,v = VhG.TnT()
 
-# Integration domains (and integration parameter "subdivlvl" and
-# "force_intorder")
-lset_if = {"levelset": lset_approx, "domain_type": IF, "subdivlvl": subdivlvl}
+# measure on surface
+ds = dCut(lset_approx, IF, definedonelements=ba_IF, deformation=deformation)
+# measure on the bulk around the surface
+dx = dx(definedonelements=ba_IF, deformation=deformation)
 
 # Bilinear forms:
 a = BilinearForm(VhG, symmetric=True)
-a += SymbolicBFI(levelset_domain=lset_if, form=P(grad(u)) * P(grad(v)) + u * v)
-a += SymbolicBFI(form=1.0 / h * (InnerProduct(grad(u), n)
-                                 * InnerProduct(grad(v), n)),
-                 definedonelements=ba_IF)
-a.Assemble()
+a += (diff_cf * P(grad(u)) * P(grad(v)) + reac_cf * u * v)*ds
+a += ((diff_cf / h + reac_cf * h) * (grad(u) * n) * (grad(v) * n))*dx
 
+# R.h.s. linear form
 f = LinearForm(VhG)
-f += SymbolicLFI(levelset_domain=lset_if, form=sin(x) * v)
+f += coef_f * v * ds
+
+a.Assemble()
 f.Assemble()
 
-gfu.vec[:] = 0.0
-gfu.vec.data = a.mat.Inverse(freedofs) * f.vec
+gfu.vec.data = a.mat.Inverse(VhG.FreeDofs()) * f.vec
 
-nan = CoefficientFunction(float('nan'))
-Draw(IfPos(cf_IF - 0.5, gfu, nan), mesh, "u")
+print("l2error : ", sqrt(Integrate((gfu - exact)**2*ds, mesh=mesh)))
+Draw(deformation, mesh, "deformation")
+Draw(gfu, mesh, "u")
 
-visoptions.mminval = -0.2
-visoptions.mmaxval = 0.2
-visoptions.deformation = 1
-visoptions.autoscale = 0
+input("Continue (press enter) to create a VTK-Output to tracefem3d.vtk")
+VTKOutput(ma=mesh, coefs=[deformation, lset_approx, gfu],
+          names=["deformation", "P1-levelset", "u"],
+          filename="tracefem", subdivision=2).Do()
