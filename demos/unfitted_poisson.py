4421cdb02f694391c6fa9b137e2aa1a24f6a9a74
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2021-02-22 20:47:40 +0100

    merge nxfem and cutfem to unfitted_poisson

--- demos/unfitted_poisson.py
+++ demos/unfitted_poisson.py
@@ -1,8 +1,8 @@
 """
 In this example we solve a scalar *unfitted* interface problem. As a
 discretisation method we use a level set based geometry description and
-a Cut Finite element method with a Nitsche formulation to impose the
-interface conditions.
+a Cut or Extended Finite element method with a Nitsche formulation to 
+impose the interface conditions.
 
 Domain:
 -------
@@ -30,8 +30,11 @@ domain-wise constants which are different in the two sub-domains.
 
 Discretisation:
 ---------------
-* Finite element space: For each sub-domain we consider the space of
-  piecewise linear restricted to the corresponding sub-domain.
+* Finite element space: We consider two different formulations:
+  * "CutFEM" For each sub-domain we consider the space of piecewise 
+    polynomials restricted to the corresponding sub-domain.
+  * "XFEM" we consider a standard space of piecewise polynomials combined
+    with an enrichment space.
 
 * Variational formulation: We use a Nitsche formulation which involves
   averages of the fluxes and jumps of the solution across the interface.
@@ -41,30 +44,16 @@ Discretisation:
 
 Implementational aspects:
 -------------------------
-* Finite element space: The finite element space is a simple composition
-  of two scalar piecewise linear finite element spaces w.r.t. the
-  background mesh. However, in the variational formulation only these
-  degrees of freedom that have some part in the corresponding sub-domain
-  are used and appear in the equations. Correspondingly, degrees of
-  freedom in the active part of the mesh and unused degrees of freedoms
-  are marked differently to obtain a consistent treatment and a regular
-  stiffness matrix.
-
 * Geometry approximation: To approximate the implicitly described
   geometry we use the piecewise (multi-) linear interpolation of the
   level set function. For this geometry approximation (arbitrary order)
   accurate numerical integration is provided.
+  For higher order approximation an additional mesh deformation is 
+  applied that maps the low order interface approximation to something
+  more accurate.
 
 * Linear systems: A (sparse) direct solver is applied to solve the
   arising linear systems.
-
-Extensions:
------------
-* Instead of using the CutFEM-characterization of the space, one could
-  use an XFEM-characterization, cf. nxfem.py.
-* To obtain higher order accuracy (also w.r.t. the geometry
-  approximation) isoparametric unfitted methods can be used, cf.
-  nxfem_higher_order.py.
 
 Literature:
 -----------
@@ -94,6 +83,7 @@ alpha = [1.0, 2.0]
 # Nitsche penalty parameter
 lambda_nitsche = 20
 
+formulation = "XFEM"  # or "CUTFEM"
 
 # ----------------------------------- MAIN ------------------------------------
 # We generate the background mesh of the domain and use a simplicity
@@ -104,22 +94,18 @@ square.AddRectangle(ll, ur, bc=1)
 mesh = Mesh(square.GenerateMesh(maxh=maxh, quad_dominated=False))
 
 # Manufactured solution and corresponding r.h.s. data CoefficientFunctions:
-r44 = x**4 + y**4
-r41 = sqrt(sqrt(x**4 + y**4))
-r4m3 = 1.0 / (r41 * r41 * r41)
-r66 = x**6 + y**6
-r63 = sqrt(r66)
 r22 = x**2 + y**2
-r21 = sqrt(r22)
+r44 = x**4 + y**4
+r66 = x**6 + y**6
+r41 = sqrt(sqrt(r44))
+r4m3 = 1.0 / r41**3
 solution = [1 + pi / 2 - sqrt(2.0) * cos(pi / 4 * r44), pi / 2 * r41]
-coef_f = [(-1 * sqrt(2) * pi * (pi * cos(pi / 4 * (r44)) * (r66)
-                                + 3 * sin(pi / 4 * (r44)) * (r22))),
-          (-2 * pi * 3 / 2 * (r4m3) * (-(r66) / (r44) + (r22)))]
-
+coef_f = [-alpha[i]*(solution[i].Diff(x).Diff(x)
+                     + solution[i].Diff(y).Diff(y)) for i in range(2)]
 
 # Level set function of the domain (phi = ||x||_4 - 1) and its
 # interpolation:
-levelset = sqrt(sqrt(x**4 + y**4)) - 1.0
+levelset = r41 - 1.0
 
 if order > 1:
     from xfem.lsetcurv import LevelSetMeshAdaptation
@@ -129,30 +115,42 @@ else:
 lsetp1 = lsetadap.lset_p1
 
 # Background FESpaces (used as CutFESpaces later-on):
-Vh = H1(mesh, order=order, dirichlet=[1, 2, 3, 4])
-VhG = FESpace([Vh, Vh])
-print("unknowns in background FESpace (2 x standard unknowns): ", VhG.ndof)
+Vh = H1(mesh, order=order, dirichlet=".*")
 
 # Gathering information on cut elements:
-#  * domain of (volume/boundary) element:
-#    * NEG: only negative level set values
-#    * POS: only positive level set values
-#    * IF: cut element (negative and positive) level set values
-#  * cut ratio:
-#    If element is cut this describes the ratio between the measure of
-#    part in the negative domain and the measure of the full element.
 ci = CutInfo(mesh, lsetp1)
 
-# Overwrite freedofs (degrees of freedoms that should be solved for) of
-# VhG to mark only dofs that are involved in the cut problem. Use cut
-# information of ci here:
-# (Note: "hasneg": has (also) negative level set values and
-#        "haspos": has (also) positive level set values)
-hasneg = ci.GetElementsOfType(HASNEG)
-haspos = ci.GetElementsOfType(HASPOS)
-freedofs = VhG.FreeDofs()
-freedofs &= CompoundBitArray([GetDofsOfElements(Vh, hasneg),
-                              GetDofsOfElements(Vh, haspos)])
+if formulation == "XFEM":
+    Vhx = XFESpace(Vh, lsetp1)
+    VhG = Vh * Vhx
+
+    gfu = GridFunction(VhG)
+
+    (u_std, u_x), (v_std, v_x) = VhG.TnT()
+
+    u = [u_std + op(u_x) for op in [neg, pos]]
+    v = [v_std + op(v_x) for op in [neg, pos]]
+    gradu = [grad(u_std) + op(u_x) for op in [neg_grad, pos_grad]]
+    gradv = [grad(v_std) + op(v_x) for op in [neg_grad, pos_grad]]
+
+    gfu_std, gfu_x = gfu.components
+    gfuh = [gfu_std + op(gfu_x) for op in [neg, pos]]
+else:
+    # Create CutFESpace by selecting only active elements for inside
+    # and outside:
+    VhG = Compress(Vh, GetDofsOfElements(Vh, ci.GetElementsOfType(HASNEG))) \
+        * Compress(Vh, GetDofsOfElements(Vh, ci.GetElementsOfType(HASPOS)))
+
+    gfu = GridFunction(VhG)
+
+    u, v = VhG.TnT()
+    gradu, gradv = [[grad(w[i]) for i in [0, 1]] for w in [u, v]]
+
+    gfuh = gfu.components
+
+print("unknowns in background FESpace : ", Vh.ndof)
+print("unknowns in " + formulation + " FESpace : ", VhG.ndof)
+
 
 # coefficients / parameters:
 n = 1.0 / grad(lsetp1).Norm() * grad(lsetp1)
@@ -162,75 +160,56 @@ kappa = (CutRatioGF(ci), 1.0 - CutRatioGF(ci))
 # Nitsche stabilization parameter:
 stab = lambda_nitsche * (alpha[1] + alpha[0]) / h
 
-# expressions of test and trial functions (u and v are tuples):
-u = VhG.TrialFunction()
-v = VhG.TestFunction()
-
-gradu = [grad(ui) for ui in u]
-gradv = [grad(vi) for vi in v]
-
 average_flux_u = sum([- kappa[i] * alpha[i] * gradu[i] * n for i in [0, 1]])
 average_flux_v = sum([- kappa[i] * alpha[i] * gradv[i] * n for i in [0, 1]])
 
 # Integration domains for integration on negative/positive sub-domains
 # and on the interface: Here, the integration is (geometrically) exact
-# if the "levelset"-argument is a piecewise (multi-)linear function. The
-# integration order is chosen according to the arguments in the
-# multi-linear forms (but can be overwritten with "force_intorder" in
-# the integration domain). If the "levelset"-argument is not a
-# (multi-)linear function, you can use the "subdivlvl" argument to add
-# additional refinement levels for the geometry approximation.
-dx = tuple([dCut(lsetp1, dt, deformation=lsetadap.deform)
+# if the "levelset"-argument is a piecewise (multi-)linear function.
+# We further provide a mesh deformation that is applied in the higher order
+# case:
+dx = tuple([dCut(lsetp1, dt, deformation=lsetadap.deform,
+                 definedonelements=ci.GetElementsOfType(HAS(dt)))
             for dt in [NEG, POS]])
 ds = dCut(lsetp1, IF, deformation=lsetadap.deform)
 
-# bilinear forms:
+# bilinear form for the unfitted Nitsche formulation:
 a = BilinearForm(VhG, symmetric=True)
-# l.h.s. domain integrals:
 a += sum(alpha[i] * gradu[i] * gradv[i] * dx[i] for i in [0, 1])
-# Nitsche integrals:
 a += (average_flux_u * (v[0] - v[1]) + average_flux_v * (u[0] - u[1])
       + stab * (u[0] - u[1]) * (v[0] - v[1])) * ds
 
+# r.h.s.:
 f = LinearForm(VhG)
-# r.h.s. domain integrals:
 f += sum(coef_f[i] * v[i] * dx[i] for i in [0, 1])
 
-# solution vector
-gfu = GridFunction(VhG)
-
+# setting domain boundary conditions:
+# the context manager lsetadap applies the mesh deformation
+# in the higher order case:
 with lsetadap:
-    # setting domain boundary conditions:
-    gfu.components[1].Set(solution[1], BND)
+    if formulation == "XFEM":
+        gfu.components[0].Set(solution[1], BND)
+    else:
+        gfu.components[1].Set(solution[1], BND)
 
 # setting up matrix and vector
 a.Assemble()
 f.Assemble()
 
 # homogenization of boundary data and solution of linear system
-rhs = gfu.vec.CreateVector()
-rhs.data = f.vec - a.mat * gfu.vec
-update = gfu.vec.CreateVector()
-update.data = a.mat.Inverse(freedofs) * rhs
-gfu.vec.data += update
-
-# visualization of (discrete) solution: Wherever (interpolated) level
-# set function is negative visualize the first component, where it is#
-# positive visualize the second component
-u_coef = IfPos(lsetp1, gfu.components[1], gfu.components[0])
-
+f.vec.data -= a.mat * gfu.vec
+gfu.vec.data += a.mat.Inverse(VhG.FreeDofs()) * f.vec
+ 
 # visualize levelset, interpolated levelset and discrete solution:
-# (Note that the visualization does not respect the discontinuities.
-# They are smeared out. To see kinks or jumps more clearly increase the
-# subdivision option in the visualization.)
 Draw(levelset, mesh, "levelset")
 Draw(lsetp1, mesh, "levelset_P1")
-Draw(u_coef, mesh, "u")
+# Note that standard netgen-gui visualization does not respect 
+# the discontinuities, they are smeared out. To see kinks or jumps 
+# more clearly increase the subdivision option in the visualization.
+DrawDC(lsetp1, gfu.components[0], gfu.components[1],
+       mesh, "u", deformation=lsetadap.deform)
 
 # Computation of L2 error:
-dx = tuple([dCut(lsetp1, dt, deformation=lsetadap.deform, order=2 * order)
-            for dt in [NEG, POS]])
-err_sqr = sum([(gfu.components[i] - solution[i])**2 * dx[i] for i in [0, 1]])
-l2error = sqrt(Integrate(err_sqr, mesh))
-
-print("L2 error : ", l2error)
+err_sqr = sum([(gfuh[i] - solution[i])**2 * dx[i].order(2*order)
+               for i in [0, 1]])
+print("L2 error : ", sqrt(Integrate(err_sqr, mesh)))
