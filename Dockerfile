a5cbd671580ca1eff58011a097eec6d21fe78f01
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2021-03-11 13:35:55 +0100

    version bump

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM schruste/ngsolve:latest
+FROM ngsxfem/ngsolve:latest
 
 ARG NB_USER=jovyan
 ARG NB_UID=1000
--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='xfem',
-    version='1.4.dev.1',
+    version='2.0.dev',
     author='Christoph Lehrenfeld',
     author_email='lehrenfeld@math.uni-goettingen.de',
     description='(ngs)xfem is an Add-on library to Netgen/NGSolve for unfitted/cut FEM.',
