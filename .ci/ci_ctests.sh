c2a3db74bd968572190c56f4a9cac316038fb4ea
Fabian Heimann <Fabian.Heimann@stud.uni-goettingen.de>
2021-10-11 21:07:31 +0200

    Whats going on?

--- .ci/ci_ctests.sh
+++ .ci/ci_ctests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -e
-# echo "pwd: ${PWD}"
-# ls -al .
+echo "pwd: ${PWD}"
+ls -al .
 # echo "cmake-tests"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PWD}/install/lib/python3/dist-packages/xfem/"
 export PYTHONPATH="${PYTHONPATH}:${PWD}/install/lib/python3/dist-packages"
