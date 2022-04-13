11431c7202db4986060d3a216b8a2af7ea6a62fb
Fabian Heimann <Fabian.Heimann@stud.uni-goettingen.de>
2022-04-13 14:03:53 +0200

    Activating 3D cut facet POS/ NEG int

--- xfem/symboliccutbfi.cpp
+++ xfem/symboliccutbfi.cpp
@@ -673,7 +673,6 @@ namespace ngfem
     lsetintdom = make_shared<LevelsetIntegrationDomain>(lsetintdom_in);
     simd_evaluate=false;
     time_order = lsetintdom_in.GetTimeIntegrationOrder();
-    cout << "The SymbolicCutFacetBilinearFormIntegrator constructor has been called. time_order = " << time_order << endl;
   }
 
   template <typename SCAL, typename SCAL_SHAPES>
@@ -704,7 +703,7 @@ namespace ngfem
     Facet2ElementTrafo transform1(eltype1, ElVertices1); 
 
     if (etfacet != ET_SEGM){
-        if (time_order > -1) throw Exception("Time order > -1 not allowed in 3D.");
+        //if (time_order > -1) throw Exception("Time order > -1 not allowed in 3D.");
         //if (lsetintdom->GetDomainType() != IF) throw Exception("cut facet bilinear form can only do volume ints on ET_SEGM");
         //if (etfacet != ET_TRIG && etfacet != ET_QUAD) throw Exception("cut facet bilinear form can do IF ints only on ET_SEGM, ET_TRIG and ET_QUAD");
     }
@@ -739,7 +738,7 @@ namespace ngfem
     {
       //if (lsetintdom->GetDomainType() != IF) throw Exception("No Neg/pos facet ints in 3D yet");
       // so far only : Codim 2 special case (3D -> 1D)
-      if (time_order > -1) throw Exception("This is not possible for space_time");
+      //if (time_order > -1) throw Exception("This is not possible for space_time");
       
       static Timer t("symbolicCutBFI - CoDim2", NoTracing);
       RegionTimer reg (t);
