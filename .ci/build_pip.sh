ab7e3e2f8ea496c49c4fbf7dfe18d40b042b3f8a
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2022-08-12 10:39:20 +0200

    try with submodules

--- .ci/build_pip.sh
+++ .ci/build_pip.sh
@@ -3,28 +3,29 @@ set -e
 
 # 38 39
 
+export ORIGINAL_PATH="$PATH"
 for pyversion in 310
 do
     #export pyversion=310
     export PYDIR="/opt/python/cp${pyversion}-cp${pyversion}/bin"
     export NGSOLVE_VERSION=`$PYDIR/python external_dependencies/ngsolve/tests/get_python_version_string_from_git.py external_dependencies/ngsolve`
-    export PATH="$PATH:$PYDIR"
-    $PYDIR/pip install ngsolve
+    export PATH="ORIGINAL_$PATH:$PYDIR"
+    pip install ngsolve
     #RUN $PYDIR/pip install -vvv .
-    $PYDIR/pip wheel -vvv .
+    pip wheel -vvv .
     rm -rf _skbuild
 
     # avx2 build:
-    $PYDIR/pip uninstall -y netgen-mesher
-    $PYDIR/pip uninstall -y ngsolve
+    pip uninstall -y netgen-mesher
+    pip uninstall -y ngsolve
     
-    $PYDIR/pip install ngsolve-avx2==$NGSOLVE_VERSION
-    NETGEN_ARCH=avx2 $PYDIR/pip wheel -vvv .
+    pip install ngsolve-avx2==$NGSOLVE_VERSION
+    NETGEN_ARCH=avx2 pip wheel -vvv .
     
 done
-$PYDIR/python .ci/fix_auditwheel_policy.py
+python .ci/fix_auditwheel_policy.py
 auditwheel repair *.whl
 rm *.whl
 
-$PYDIR/pip install -U twine
-$PYDIR/twine upload wheelhouse/*manylinux*.whl
+pip install -U twine
+twine upload wheelhouse/*manylinux*.whl
