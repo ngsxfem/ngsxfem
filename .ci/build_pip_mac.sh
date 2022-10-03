6600bf1d41fc14d1e42fdd986c11434c4fbe94c6
Henry v. Wahl <henry.wahl@univie.ac.at>
2022-10-03 16:03:58 +0200

    getting there slowly

--- .ci/build_pip_mac.sh
+++ .ci/build_pip_mac.sh
@@ -14,6 +14,4 @@ export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$NGSolve_Dir:$NETGEN_Dir
 export CMAKE_OSX_ARCHITECTURES='x86_64'
 
 $PYDIR/pip wheel -vvv -w dist .
-
-ls 
-cd dist; ls
+cd dist; rm netgen*.whl ngsolve*.whl
