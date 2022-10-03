c619bf56c85940eae1f7fc791d4d293fc8dd9122
Henry v. Wahl <henry.wahl@univie.ac.at>
2022-10-03 13:06:51 +0200

    test installed ngsolve version

--- .ci/build_pip_mac.sh
+++ .ci/build_pip_mac.sh
@@ -7,6 +7,8 @@ $Python3_ROOT_DIR/python -m venv venv_ngs
 source venv_ngs/bin/activate
 $Python3_ROOT_DIR/bin/pip3 install ngsolve twine wheel pybind11-stubgen
 
+$Python3_ROOT_DIR/python -c "from ngsolve import *"
+
 export NETGEN_dir=$Python3_ROOT_DIR/lib/python$1/site-packages/netgen/cmake
 export NGSolve_Dir=$Python3_ROOT_DIR/lib/python$1/site-packages/ngsolve/cmake
 export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$NGSolve_Dir:$NETGEN_dir
