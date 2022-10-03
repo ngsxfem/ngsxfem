da7a19f211f95f1526763f823d9ca1b24041636b
Henry v. Wahl <henry.wahl@univie.ac.at>
2022-10-03 12:39:30 +0200

    check if ngsolve installed

--- .ci/build_pip_mac.sh
+++ .ci/build_pip_mac.sh
@@ -7,6 +7,8 @@ $Python3_ROOT_DIR/python -m venv venv_ngs
 source venv_ngs/bin/activate
 $Python3_ROOT_DIR/bin/pip3 install ngsolve twine wheel pybind11-stubgen
 
-export CMAKE_OSX_ARCHITECTURES='x86_64'
-$Python3_ROOT_DIR/python setup.py bdist_wheel --plat-name macosx-10.15_x86_64
-$Python3_ROOT_DIR/python -m twine upload  --repository testpypi dist/*.whl
+cd $Python3_ROOT_DIR/lib/python$1/site-packages; ls
+
+# export CMAKE_OSX_ARCHITECTURES='x86_64'
+# $Python3_ROOT_DIR/python setup.py bdist_wheel --plat-name macosx-10.15_x86_64
+# $Python3_ROOT_DIR/python -m twine upload  --repository testpypi dist/*.whl
