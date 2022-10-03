956b2a448835a4e136e1995b188ad081187ad2b8
Henry v. Wahl <henry.wahl@univie.ac.at>
2022-10-03 13:22:01 +0200

    get rid of virtual environment

--- .ci/build_pip_mac.sh
+++ .ci/build_pip_mac.sh
@@ -2,9 +2,6 @@
 set -e
 
 $Python3_ROOT_DIR/python --version
-$Python3_ROOT_DIR/python -m venv venv_ngs
-
-source venv_ngs/bin/activate
 $Python3_ROOT_DIR/bin/pip3 install ngsolve twine wheel pybind11-stubgen
 
 $Python3_ROOT_DIR/python -c "from ngsolve import *"
