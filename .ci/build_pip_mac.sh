7ad351df99e1b7c50835f8d78a2ee828a85cbf00
Henry v. Wahl <henry.wahl@univie.ac.at>
2022-10-03 12:24:47 +0200

    next try

--- .ci/build_pip_mac.sh
+++ .ci/build_pip_mac.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 set -e
 
-export PYDIR=/usr/local/Frameworks/Python.framework/Versions/$1/bin
-
-$PYDIR/python3 --version
-$PYDIR/python3 -m venv ../venv_ngs
+$Python3_ROOT_DIR/python3 --version
+$Python3_ROOT_DIR/python3 -m venv ../venv_ngs
 
 source ../venv_ngs/bin/activate
-$PYDIR/pip3 install ngsolve twine wheel
+$Python3_ROOT_DIR/pip3 install ngsolve twine wheel
 
 export CMAKE_OSX_ARCHITECTURES='x86_64'
-$PYDIR/python3 setup.py bdist_wheel --plat-name macosx-10.15_x86_64 -j3
-$PYDIR/python3 -m twine upload  --repository testpypi dist/*.whl
+$Python3_ROOT_DIR/python3 setup.py bdist_wheel --plat-name macosx-10.15_x86_64 -j3
+$Python3_ROOT_DIR/python3 -m twine upload  --repository testpypi dist/*.whl
