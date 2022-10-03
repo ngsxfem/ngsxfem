03dfab3166ec28b35dfc077de037436209615649
Henry v. Wahl <henry.wahl@univie.ac.at>
2022-10-03 12:48:27 +0200

    NGSolve_Dir

--- .ci/build_pip_mac.sh
+++ .ci/build_pip_mac.sh
@@ -7,8 +7,7 @@ $Python3_ROOT_DIR/python -m venv venv_ngs
 source venv_ngs/bin/activate
 $Python3_ROOT_DIR/bin/pip3 install ngsolve twine wheel pybind11-stubgen
 
-cd $Python3_ROOT_DIR/lib/python$1/site-packages; ls
-
-# export CMAKE_OSX_ARCHITECTURES='x86_64'
-# $Python3_ROOT_DIR/python setup.py bdist_wheel --plat-name macosx-10.15_x86_64
-# $Python3_ROOT_DIR/python -m twine upload  --repository testpypi dist/*.whl
+export NGSolve_Dir=$Python3_ROOT_DIR/lib/python$1/site-packages/ngsolve/cmake
+export CMAKE_OSX_ARCHITECTURES='x86_64'
+$Python3_ROOT_DIR/python setup.py bdist_wheel --plat-name macosx-10.15_x86_64
+$Python3_ROOT_DIR/python -m twine upload  --repository testpypi dist/*.whl
