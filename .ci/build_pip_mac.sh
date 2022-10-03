24c5e6ee83c6f04cc3b29a7976be0eb2611305a2
Henry v. Wahl <henry.wahl@univie.ac.at>
2022-10-03 14:08:34 +0200

    test ngsolve install location

--- .ci/build_pip_mac.sh
+++ .ci/build_pip_mac.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 set -e
 
-$Python3_ROOT_DIR/python --version
-$Python3_ROOT_DIR/bin/pip3 install ngsolve twine wheel pybind11-stubgen
+export PYDIR=$Python3_ROOT_DIR/bin
+$PYDIR/python3 --version
+$PYDIR/pip3 install ngsolve twine wheel pybind11-stubgen
 
-$Python3_ROOT_DIR/python -c "from ngsolve import *"
 
-export NETGEN_dir=$Python3_ROOT_DIR/lib/python$1/site-packages/netgen/cmake
-export NGSolve_Dir=$Python3_ROOT_DIR/lib/python$1/site-packages/ngsolve/cmake
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$NGSolve_Dir:$NETGEN_dir
+export NETGEN_Dir=$PYDIR/../lib/python$1/site-packages/netgen/cmake
+export NGSolve_Dir=$PYDIR/../lib/python$1/site-packages/ngsolve/cmake
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$NGSolve_Dir:$NETGEN_Dir
 export CMAKE_OSX_ARCHITECTURES='x86_64'
-$Python3_ROOT_DIR/python setup.py bdist_wheel --plat-name macosx-10.15_x86_64
-$Python3_ROOT_DIR/python -m twine upload --username __token__ --repository testpypi dist/*.whl
+
+cd $NGSolve_Dir
+ls -lta
+
+# $PYDIR/python setup.py bdist_wheel --plat-name macosx-10.15_x86_64
+# $PYDIR/python -m twine upload --username __token__ --repository testpypi dist/*.whl
