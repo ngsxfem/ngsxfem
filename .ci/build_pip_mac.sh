c67f55665f48b60472b050f300b48787c0e8350c
Henry v. Wahl <henry.wahl@univie.ac.at>
2022-10-03 12:58:49 +0200

    find netgen

--- .ci/build_pip_mac.sh
+++ .ci/build_pip_mac.sh
@@ -7,8 +7,9 @@ $Python3_ROOT_DIR/python -m venv venv_ngs
 source venv_ngs/bin/activate
 $Python3_ROOT_DIR/bin/pip3 install ngsolve twine wheel pybind11-stubgen
 
+export NETGEN_dir=$Python3_ROOT_DIR/lib/python$1/site-packages/netgen/cmake
 export NGSolve_Dir=$Python3_ROOT_DIR/lib/python$1/site-packages/ngsolve/cmake
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$NGSolve_Dir
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$NGSolve_Dir:$NETGEN_dir
 export CMAKE_OSX_ARCHITECTURES='x86_64'
 $Python3_ROOT_DIR/python setup.py bdist_wheel --plat-name macosx-10.15_x86_64
 $Python3_ROOT_DIR/python -m twine upload  --repository testpypi dist/*.whl
