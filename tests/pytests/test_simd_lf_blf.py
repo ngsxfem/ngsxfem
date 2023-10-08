5c7db1c5df67b67beb3d34fac374c5baa44354f0
Maximilian Zienecker <m.zienecker@stud.uni-goettingen.de>
2023-10-08 18:20:52 +0200

    add test for CalcElementMatrixEBAdd

--- tests/pytests/test_simd_lf_blf.py
+++ tests/pytests/test_simd_lf_blf.py
@@ -360,3 +360,151 @@ def test_calc_linearized():
     diff = Norm(w1)
     print("error: ",diff)
     assert diff < 1e-12
+
+
+# copy from test_straightcutrule.py - this test compares the results SIMD to non-SIMD
+@pytest.mark.parametrize("i", [2, 3, 4])
+def test_eb_cut_integrator_2d(i):
+    from netgen.geom2d import SplineGeometry
+
+    ngsglobals.msg_level = 1
+
+    levelset = (x -1) **4 + (y -1)**4 + 12*y + 1.4*(x -2)**3 - 15
+    len_box = 4
+
+    exact = sin(y)
+    exact_grad = CoefficientFunction((0, cos(y)))
+
+    order = 5
+
+    l2errors = []
+    h1errors = []
+
+    extend_Fh = False
+    condense = True
+
+    square = SplineGeometry()
+    square.AddRectangle([-len_box,-len_box],[len_box,len_box],bc=1)
+    mesh = Mesh (square.GenerateMesh(maxh=len_box/1.5*0.5**(i), quad_dominated=False))
+    print("Max_h = ", len_box/1.5*0.5**(i))
+
+    lsetmeshadap = LevelSetMeshAdaptation(mesh, order=order, threshold=10.25, discontinuous_qn=True)
+    deformation = lsetmeshadap.CalcDeformation(levelset)
+    lsetp1 = lsetmeshadap.lset_p1
+            
+    # TraceFESpace 
+    fes1 = L2(mesh, order=order)
+    fes2 = FacetFESpace(mesh, order=order)
+    fes3 = FacetFESpace(mesh, order=order - 1)
+        
+    Vhg = FESpace([fes1, fes2, fes3], dgjumps=not condense)
+        
+    # overwrite freedofs of VhG to mark only dofs that are involved in the cut problem
+    ci = CutInfo(mesh, lsetp1)
+    reg_Th = ci.GetElementsOfType(IF)   
+    reg_Fh = GetFacetsWithNeighborTypes(mesh,a=reg_Th,b=reg_Th,use_and=not extend_Fh)
+
+    gf_reg_Fh = GridFunction(FacetFESpace(mesh,order=0))
+    for i in range(len(reg_Fh)):
+        gf_reg_Fh.vec[i] = 1 if reg_Fh[i] else 0
+        
+    if not extend_Fh:
+            if not condense:
+                freedofs = CompoundBitArray( [GetDofsOfElements(fes1,reg_Th),  GetDofsOfFacets(fes2,reg_Fh) ,  GetDofsOfFacets(fes3,reg_Fh) ] )
+            else:
+                freedofs = CompoundBitArray( [fes1.FreeDofs(True) & GetDofsOfElements(fes1,reg_Th),  GetDofsOfFacets(fes2,reg_Fh) ,  GetDofsOfFacets(fes3,reg_Fh) ] )
+    else:
+            if not condense:
+                freedofs = GetDofsOfElements(Vhg,reg_Th)
+            else:
+                freedofs = CompoundBitArray( [fes1.FreeDofs(True) & GetDofsOfElements(fes1,reg_Th),  GetDofsOfElements(fes2,reg_Th) ,  GetDofsOfElements(fes3,reg_Th) ] )
+        
+    normal_helper_gf = GridFunction(HDiv(mesh, order=0))
+    for i in range(len(normal_helper_gf.vec)):
+        normal_helper_gf.vec[i] = 1
+    
+    gfu = GridFunction(Vhg)
+        
+    #tangential projection to given normal
+    def P(u,n_phi):
+        return u - (u*n_phi)*n_phi
+    
+    #normalization (pointwise) of a vector
+    def Normalized(u):
+        return 1.0 / Norm(u) * u
+    
+    n_phi1 = Normalized(grad(lsetp1))
+    n_phi2 = Normalized(grad(lsetp1).Other())
+    
+    h = specialcf.mesh_size
+    n_F = specialcf.normal(2)
+    
+    conormal1 = Normalized(P(n_F,n_phi1))
+    conormal2 = Normalized(P(-n_F,n_phi2))
+    
+    normal_helper_proj = InnerProduct(normal_helper_gf, n_F)*n_F
+    normal_helper = Normalized(normal_helper_proj)
+        
+    def jump(u, uhat):
+        return u - uhat
+    
+    beta_E = 4 * (order+1)**2
+    beta_F = 100.
+    beta_F2 = 1.
+    lam_nd = 0.0 if order == 1 else 0.1*1./h + 0.1*h
+    
+    u, uhat, sigmahat = Vhg.TrialFunction()
+    v, vhat, tauhat = Vhg.TestFunction()
+    lset_if  = { "levelset" : lsetp1, "domain_type" : IF , "subdivlvl" : 0}
+    
+    a = RestrictedBilinearForm(Vhg,"a",reg_Th,reg_Fh,check_unused=False, flags={"eliminate_internal": condense})
+    a += SymbolicBFI(levelset_domain = lset_if, form = P(grad(u),n_phi1) * P(grad(v),n_phi1) + u * v, definedonelements=reg_Th)
+    a += SymbolicBFI(form = (lam_nd * grad(u)*n_phi1) * (grad(v)*n_phi1), definedonelements=reg_Th)
+    
+    a += SymbolicBFI(levelset_domain = lset_if, form = ( - InnerProduct(grad(u),conormal1)*jump(v, vhat)
+                                                            - InnerProduct(grad(v),conormal1)*jump(u, uhat)
+                                                            + beta_E/h * (u- uhat)*(v - vhat) ) *gf_reg_Fh,
+                                                            element_boundary=True, definedonelements=reg_Th)
+    
+    a += SymbolicBFI(form = beta_F/(h*h) * (u- uhat) * (v - vhat) *gf_reg_Fh, element_boundary=True, definedonelements=reg_Th)
+
+    a += SymbolicBFI(form = beta_F2 * (grad(u)*normal_helper- sigmahat) * (grad(v)*normal_helper - tauhat) *gf_reg_Fh, element_boundary=True, definedonelements=reg_Th)
+
+    f_coeff = -(4*(y - 1)**3 + 12)**2*((4.2*(x - 2)**2 + 4*(x - 1)**3)**2 + (4*(y - 1)**3 + 12)**2)**(-1.0)*sin(y) + (4*(y - 1)**3 + 12)*((4.2*(x - 2)**2 + 4*(x - 1)**3)**2 + (4*(y - 1)**3 + 12)**2)**(-0.5)*(-12.0*(y - 1)**2*(4*(y - 1)**3 + 12)**2*((4.2*(x - 2)**2 + 4*(x - 1)**3)**2 + (4*(y - 1)**3 + 12)**2)**(-1.5) + 12*(y - 1)**2*((4.2*(x - 2)**2 + 4*(x - 1)**3)**2 + (4*(y - 1)**3 + 12)**2)**(-0.5) - 0.5*(4.2*(x - 2)**2 + 4*(x - 1)**3)**2*((4.2*(x - 2)**2 + 4*(x - 1)**3)**2 + (4*(y - 1)**3 + 12)**2)**(-1.5)*(16.8*x + 24*(x - 1)**2 - 33.6) + ((4.2*(x - 2)**2 + 4*(x - 1)**3)**2 + (4*(y - 1)**3 + 12)**2)**(-0.5)*(8.4*x + 12*(x - 1)**2 - 16.8))*cos(y) + 2*sin(y)
+        
+    f = LinearForm(Vhg)
+    f += SymbolicLFI(levelset_domain = lset_if, form = f_coeff * v, definedonelements=reg_Th)
+
+    mesh.SetDeformation(deformation)
+    a.Assemble()
+    f.Assemble();
+        
+    gfu.vec[:] = 0.0
+    if not condense:
+        gfu.vec.data = a.mat.Inverse(freedofs, "sparsecholesky") * f.vec
+    else:
+        f.vec.data += a.harmonic_extension_trans * f.vec 
+        inv = a.mat.Inverse(freedofs, "sparsecholesky")
+        gfu.vec.data = inv * f.vec
+        
+        gfu.vec.data += a.harmonic_extension * gfu.vec
+        gfu.vec.data += a.inner_solve * f.vec
+        
+    err_sqr_coefs = (gfu.components[0]-exact)**2
+
+    ngsxfemglobals.SwitchSIMD(True)
+    l2error_simd = sqrt( Integrate( levelset_domain=lset_if, cf=err_sqr_coefs, mesh=mesh, order=2*order+1) )
+        
+    ngsxfemglobals.SwitchSIMD(False)
+    l2error_ns = sqrt( Integrate( levelset_domain=lset_if, cf=err_sqr_coefs, mesh=mesh, order=2*order+1) )
+        
+    H1_err_sqr_coefs = Norm( P (gfu.components[0].Deriv() - exact_grad, n_phi1) )**2
+    ngsxfemglobals.SwitchSIMD(True)
+    h1error_simd = sqrt( l2error_simd**2 + Integrate( levelset_domain=lset_if, cf=H1_err_sqr_coefs, mesh=mesh, order=2*order+1) )
+    ngsxfemglobals.SwitchSIMD(False)
+    h1error_ns = sqrt( l2error_ns**2 + Integrate( levelset_domain=lset_if, cf=H1_err_sqr_coefs, mesh=mesh, order=2*order+1) )
+
+    assert abs(l2error_simd - l2error_ns) < 1e-12
+    assert abs(h1error_simd - h1error_ns) < 1e-12
+
+    mesh.UnsetDeformation()
