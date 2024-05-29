d2049d206ed9df53c4516e6f6190c3e5be21af9e
Maximilian Zienecker <m.zienecker@stud.uni-goettingen.de>
2024-05-29 19:13:41 +0200

    try to fix pages

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -54,22 +54,11 @@ def get_version():
 
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'ngsxfem'
 copyright = '2022, Christoph Lehrenfeld and the ngsxfem developer'
@@ -79,109 +68,30 @@ author = 'Christoph Lehrenfeld, Fabian Heimann, Henry von Wahl, Janosch Preuß a
 release = get_version()
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-# extensions = [ "myst_nb" ]
-extensions = ["sphinx.ext.autodoc","sphinx.ext.mathjax","sphinx.ext.todo","sphinx.ext.githubpages",
-              "IPython.sphinxext.ipython_console_highlighting", "IPython.sphinxext.ipython_directive",
-              # "jupyter_sphinx.execute",
-              "jupyter_sphinx",
-              "nbsphinx",
-              "m2r2",
-              "sphinxemoji.sphinxemoji",
-              ]
-# source_suffix = ['.rst', '.md']
+extensions = [
+    "sphinx_mdinclude",
+    "jupyter_sphinx",
+    "sphinx.ext.autodoc",
+    # "nbsphinx"
+    ]
 
-# Run notebook configuration
-
-# The template used when exporting from nbconvert
-#   full  - Outputs the full HTML document [Default]
-#   basic - Outputs a single div (with no additional resources)
-run_notebook_export_template = 'basic'  # Default: 'full'
-
-# Display the source links to the generated evaluated files
-run_notebook_display_source_links = False  # Default: True
-
-# Whether or not to evaluate the notebooks prior to embedding them
-evaluate_notebooks = True  # Default: True
-
-# START nbsphinx stuff
-#increase timeout for cell execution, since some files take long to execute
-nbsphinx_timeout = 100000
-
-# If True, the build process is continued even if an exception occurs:
-nbsphinx_allow_errors = False
-
-# This is processed by Jinja2 and inserted before each notebook
-nbsphinx_prolog = r"""
-.. raw:: html
-
-    <style>
-        .p-Widget {
-            height: 400px;
-        }
-        .dg.main {
-            margin-left: 0px;
-        }
-        div.p-Widget div div div div.dg ul li {
-            list-style: none;
-            margin-left: 0px;
-        }
-        div.p-Widget div div div div.dg ul li div.dg {
-            margin-bottom: 0px;
-        }
-    </style>
-
-.. only:: html
-    .. role:: raw-html(raw)
-        :format: html
-"""
-
-nbsphinx_widgets_path = ""
-# END nbsphinx stuff
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
-
-# If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = False
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+exclude_patterns = []
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'paper','env']
 
 
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-#html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-html_css_files = ['bullets.css']
-
-# html_theme_options = {
-#     'github_user': 'ngsxfem',
-#     'github_repo': 'ngsxfem',
-#     # 'github_banner':True,
-#     # 'travis_button':True,
-#     'github_button':True,
-#     'fixed_sidebar':True,
-#     'font_size':10
-#     # 'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
-#     # 'analytics_anonymize_ip': False,
-# }
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 master_doc = "contents"
 
-# m2r_parse_relative_links = True
+source_suffix = ['.rst', '.md']
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = ['bullets.css']
 
 html_sidebars = {
    'index': [
@@ -194,14 +104,53 @@ html_sidebars = {
        ],
 }
 
-nbsphinx_allow_errors = True
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-# html_js_files = ['webgui_jupyter_widgets.js', 'webgui.js', 'tentswebgui.js']
-
 def setup(app):
     app.add_css_file("custom.css")
+
+
+#-------nbsphinx (and probably also other) stuff
+# The template used when exporting from nbconvert
+#   full  - Outputs the full HTML document [Default]
+#   basic - Outputs a single div (with no additional resources)
+# run_notebook_export_template = 'basic'  # Default: 'full'
+
+# # Display the source links to the generated evaluated files
+# run_notebook_display_source_links = False  # Default: True
+
+# # Whether or not to evaluate the notebooks prior to embedding them
+# evaluate_notebooks = True  # Default: True
+
+# # START nbsphinx stuff
+# #increase timeout for cell execution, since some files take long to execute
+# nbsphinx_timeout = 100000
+
+# # If True, the build process is continued even if an exception occurs:
+# nbsphinx_allow_errors = False
+
+# # This is processed by Jinja2 and inserted before each notebook
+# nbsphinx_prolog = r"""
+# .. raw:: html
+
+#     <style>
+#         .p-Widget {
+#             height: 400px;
+#         }
+#         .dg.main {
+#             margin-left: 0px;
+#         }
+#         div.p-Widget div div div div.dg ul li {
+#             list-style: none;
+#             margin-left: 0px;
+#         }
+#         div.p-Widget div div div div.dg ul li div.dg {
+#             margin-bottom: 0px;
+#         }
+#     </style>
+
+# .. only:: html
+#     .. role:: raw-html(raw)
+#         :format: html
+# """
+
+# nbsphinx_widgets_path = ""
+# # END nbsphinx stuff
