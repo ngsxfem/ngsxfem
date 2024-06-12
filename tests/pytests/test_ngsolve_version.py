50e9dfbefea47185a8dcd1acd2f140447812197a
Christoph Lehrenfeld <christoph.lehrenfeld@gmail.com>
2024-06-12 06:13:59 +0200

    fix ngsolve version check test

--- tests/pytests/test_ngsolve_version.py
+++ tests/pytests/test_ngsolve_version.py
@@ -1,5 +1,5 @@
 import xfem
-from xfem import check_if_ngsolve_newer_than
+from xfem.ngs_check import check_if_ngsolve_newer_than, __ngsolve_required__
 
 def test_ngsolve_version():
     assert(check_if_ngsolve_newer_than(xfem.__ngsolve_required__))
